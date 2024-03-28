f1bc1a8ae55c13d6422e4843a0630427e57ecc3d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-03-28 11:49:04 -0300

    Create insert.sql
    insert de banco bdLivraria

---
+++ BancoDeDados/2ano/1bim/bdLivraria/insert.sql
@@ -0,0 +1,23 @@
+   INSERT INTO tbAutor
+    (nomeAutor)
+	  VALUES
+	   ('Machado de Assis')
+	   ,('Cora Coralina')
+	   ,('Graciliano Ramos')
+	   ,('Clarice Lispector')
+	   ,('Erico Verissimo')
+
+   INSERT INTO tbEditora
+    (nomeEditora)
+	  VALUES
+	   ('Cia das Letra')
+	   ,('Atica')
+	   ,('Saraiva')
+
+   INSERT INTO tbGenero
+     (nomeGenero)
+	   VALUES
+	     ('Romance')
+		 ,('Posia')
+		 ,('Literatura Brasileira')
+	--chatGPT/15livros, 3autor, 5editora, 5genero--
