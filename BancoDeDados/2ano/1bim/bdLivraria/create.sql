528e611462d8a1faea26af121a551a86fc66d5e0
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-03-28 11:32:47 -0300

    Create create.sql
    bd livraria slq

---
+++ BancoDeDados/2ano/1bim/bdLivraria/create.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE bdLivrariaBrasileira
+--DROP DATABASE--
+CREATE TABLE tbAutor(
+   idAutor INT PRIMARY KEY IDENTITY(1,1)
+   ,nomeAutor VARCHAR (70) NOT NULL
+   )
+
+CREATE TABLE tbEditora(
+   idEditora INT PRIMARY KEY IDENTITY(1,1)
+   ,nomeEditora VARCHAR (70) NOT NULL
+   )
+
+CREATE TABLE tbGenero(
+   idGenero INT PRIMARY KEY IDENTITY(1,1)
+   ,nomeGenero VARCHAR (70) NOT NULL
+   )
+
+CREATE TABLE tbLivro(
+   idLivro INT PRIMARY KEY IDENTITY(1,1)
+   ,nomeLivro VARCHAR (70) NOT NULL
+   ,numPaginas VARCHAR (5) NOT NULL
+   ,idGenero INT FOREIGN KEY REFERENCES tbGenero(idGenero)
+   ,idAutor INT FOREIGN KEY REFERENCES tbAutor(idAutor)
+   ,idEditora INT FOREIGN KEY REFERENCES tbEditora(idEditora)
+   )
+
