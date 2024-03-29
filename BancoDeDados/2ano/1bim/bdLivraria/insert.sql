73a85c22a45781e0e643cb45e122ea59df8e5a51
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-03-28 23:03:17 -0300

    Update insert.sql

--- BancoDeDados/2ano/1bim/bdLivraria/insert.sql
+++ BancoDeDados/2ano/1bim/bdLivraria/insert.sql
@@ -1,4 +1,4 @@
-   INSERT INTO tbAutor
+    INSERT INTO tbAutor
     (nomeAutor)
 	  VALUES
 	   ('Machado de Assis')
@@ -13,7 +13,7 @@
 	   ('Cia das Letra')
 	   ,('Atica')
 	   ,('Saraiva')
-
+	   
    INSERT INTO tbGenero
      (nomeGenero)
 	   VALUES
@@ -24,22 +24,20 @@
 	-- Inserir 3 livros para cada autor
 -- Inserir 3 livros para cada autor
 -- Inserir livros para cada autor, editora e gênero
-INSERT INTO tbLivro (nomeLivro, numPaginas, idGenero, idAutor, idEditora)
-VALUES 
-    ('Dom Casmurro', 300, 1, 1, 1),
-    ('Memórias Póstumas de Brás Cubas', 250, 1, 1, 1),
-    ('Quincas Borba', 200, 1, 1, 1),
-    ('Meu livro de cordel', 150, 2, 2, 2),
-    ('Vintém de cobre', 180, 2, 2, 2),
-    ('Estórias da Casa Velha da Ponte', 120, 2, 2, 2),
-    ('Vidas Secas', 220, 3, 3, 3),
-    ('Angústia', 180, 3, 3, 3),
-    ('São Bernardo', 200, 3, 3, 3),
-    ('A hora da estrela', 180, 1, 4, 1),
-    ('A paixão segundo G.H.', 160, 1, 4, 1),
-    ('Laços de família', 150, 1, 4, 1),
-    ('O tempo e o vento', 400, 1, 5, 1),
-    ('Incidente em Antares', 350, 1, 5, 1),
-    ('Clarissa', 320, 1, 5, 1);
-
-	--EXERCÍCIOS--
+   INSERT INTO tbLivro (nomeLivro, numPaginas, idGenero, idAutor, idEditora)
+    VALUES 
+     ('Dom casmurro', 300, 1, 1, 1),
+     ('Memórias póstumas de brás cubas', 250, 1, 1, 1),
+     ('Quincas borba', 200, 1, 1, 1),
+     ('Meu livro de cordel', 150, 2, 2, 2),
+     ('Vintém de cobre', 180, 2, 2, 2),
+     ('Estórias da casa velha da ponte', 120, 2, 2, 2),
+     ('Vidas secas', 220, 3, 3, 3),
+     ('Angústia', 180, 3, 3, 3),
+     ('São bernardo', 200, 3, 3, 3),
+     ('A hora da estrela', 180, 1, 4, 1),
+     ('A paixão segundo g.h.', 160, 1, 4, 1),
+     ('Laços de família', 150, 1, 4, 1),
+     ('O tempo e o vento', 400, 1, 5, 1),
+     ('Incidente em antares', 350, 1, 5, 1),
+     ('Clarissa', 320, 1, 5, 1);
