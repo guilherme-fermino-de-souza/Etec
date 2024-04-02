c7c4e6fb7e9f5f5e95c5f13ce129791217b1138a
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-01 22:46:02 -0300

    Create navbar.php

---
+++ PW2/1°bim/aula2/Formulario/Gworks/navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nav</title>
+    <link href="nav.css" rel="stylesheet">
+</head>
+<body>
+    <section id = "containerNav">        <!--Começo conteiner nav-->
+        <div class = "logo">  <!--logo sustentável-->
+            <i><img src="./img/img-imc-logo.avif"></i>
+        </div>
+        
+        <div class = "navbarLinks">  <!--Começo links-->
+            <nav>
+                <ul>
+                    <li><a href="../auladois/cliente.php">Home</a></li>
+                    <li><a href="#">Sobre nós</a></li>
+                    <li><a href="#">Atendimento</a></li>
+                    <li><a href="#">Periódicos</a></li>
+                </ul>
+            </nav>
+        </div>  <!--Fim links nav-->
+
+          <!--  <div class = "btn-cadastro">    <!--Começo botão
+                <a href = "#"><button>Cadastra-te</button></a>
+            </div>  -->
+
+    </section>
+</body>
+</html>
