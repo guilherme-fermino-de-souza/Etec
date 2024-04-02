cf4e975d39784fa9fe27fa29438469faa8789c37
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-01 22:48:36 -0300

    Create footer.php

---
+++ PW2/1°bim/aula2/Formulario/Gworks/footer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="foot.css">
+</head>
+
+<body>
+    <section id="footer">
+        <h2>Siga-nos:</h2>
+        <div class="redes">
+            <nav>
+                <ul>
+                    <li> <a href="#"> Instagram </a> <img src="./img/instagram.png" alt="Logo Instagram"> </li>
+                    <li> <a href="#">Twitter </a> <img src="./img/twitter.png" alt="Logo Twitter"> </li>
+                    <li> <a href="#">Facebook </a> <img src="./img/facebook.png" alt="Logo Facebook"> </li>
+                    <li> <a href="#">Linkedin </a> <img src="./img/linkedin.png" alt="Logo Linkedin"> </li>
+                </ul>
+            </nav>
+        </div>
+
+        <h1>@Absolute Medicine</h1>
+
+        <h2>©Todos os direitos reservados</h2>
+
+    </section>
+</body>
+
+</html>
