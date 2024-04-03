3c3c22996608b3844739aab5aa58701564601ad6
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-03 20:00:16 -0300

    Create InserirDados.java

---
+++ java/2°ano/1°bim/aula4/teste/InserirDados.java
@@ -0,0 +1,12 @@
+package aula4;
+
+public class InserirDados {
+
+	public static void main(String[] args) {
+		//Instância
+		Calculadora c1= new Calculadora();
+		c1.InserirDados();
+		c1.Calculo(c1.tipo); // chamada do método Calculo
+	}
+
+}
