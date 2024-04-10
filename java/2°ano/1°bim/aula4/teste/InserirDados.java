99e6735a7e83a49dd43fe15d81a18cd74da1fff7
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-10 11:48:18 -0300

    Update InserirDados.java

--- java/2°ano/1°bim/aula4/teste/InserirDados.java
+++ java/2°ano/1°bim/aula4/teste/InserirDados.java
@@ -6,7 +6,25 @@ public class InserirDados {
 		//Instância
 		Calculadora c1= new Calculadora();
 		c1.InserirDados();
-		c1.Calculo(c1.tipo); // chamada do método Calculo
+		
+		/*c1.Calculo(c1.tipo); // chamada do método Calculo*/
+		
+		if (c1.tipo==1) {
+			c1.Soma();
+			
+		} else if (c1.tipo==2) {		
+			c1.Subtracao();
+			
+		} else if (c1.tipo==3) {			
+			c1.Multiplicacao();
+			
+		} else if (c1.tipo==4) {
+			c1.Divisao();
+			
+		} else {
+			System.out.println("Número inválido");
+		}
+		
 	}
 
 }
