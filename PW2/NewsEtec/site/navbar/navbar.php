e3d137c54c0ba337d7657dcf0b4092ba18c14593
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-16 20:53:02 -0300

    Create navbar.php

---
+++ PW2/NewsEtec/site/navbar/navbar.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="../site/navbar.css">
+</head>
+
+<body>
+
+    <!--navbar start-->  <!--INÍCIO--> 
+    <header>
+        <nav>
+            <a class="logo" href="../site/index.php">NEWS WALL ETEC</a>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="#conteudo">Início</a></li>
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#cards">Integrantes</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!--navbar end-->
+
+</body>
+
+</html>
