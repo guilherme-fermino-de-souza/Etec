dca186160f4c028c915eeb7c28e1eaf1ae6ba450
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-19 21:22:12 -0300

    Update and rename main to main.java

---
+++ java/2°ano/1°bim/aula5/Pares/main.java
@@ -0,0 +1,12 @@
+package aula4;
+
+public class Quest2 {
+
+	public static void main(String[] args) {
+		// Método sem retorno e sem parâmetro que exiba os números pares de 0 a 100
+		
+		ExibirNumeros en = new ExibirNumeros();
+		
+		en.Pares();
+	}
+}
