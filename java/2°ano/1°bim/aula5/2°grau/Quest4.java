3cc159afb178a98c403d7c182414e288446d3607
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-20 14:02:26 -0300

    Create Quest4.java

---
+++ java/2°ano/1°bim/aula5/2°grau/Quest4.java
@@ -0,0 +1,19 @@
+package aula4;
+
+import java.util.Scanner;
+
+public class Quest4 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		// Método sem retorno e com parâmetro que calcule as raízes de uma equação do segundo grau
+		
+		//Escolha e = new Escolha();
+		Calculo ce = new Calculo();
+    
+		ce.Raizes(0, 0, 0, 0, 0, 0);
+		
+		
+	}
+
+}
