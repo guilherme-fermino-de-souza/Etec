c2faa3f64edab7be0fb5754df5e50282ee40ee77
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-23 03:45:13 -0300

    Create main.java

---
+++ java/2°ano/1°bim/aula5/Fatorial/main.java
@@ -0,0 +1,17 @@
+package aula4;
+
+import java.util.Scanner;
+
+public class QuestTwo {
+	
+	public static void main(String[] args) {
+		
+	
+	//Método com retorno e com parâmetro que exiba o fatorial de um número;
+	Scanner in = new Scanner(System.in);
+	
+	Fatorial f = new Fatorial();
+	f.Conta(0, 0);
+		
+ }
+}
