56766760a87e57818cea17ba1929252108e7a24d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-23 02:16:22 -0300

    Create main.java

---
+++ java/2°ano/1°bim/aula5/Triangulo/main.java
@@ -0,0 +1,16 @@
+package aula4;
+
+import java.util.Scanner;
+
+public class Quest1 {
+
+	public static void main(String[] args) {
+		//Método com retorno e com parêmetro que informe se 3 valores formam um triângulo
+		Scanner in = new Scanner(System.in);
+		
+		Triangulo t = new Triangulo();
+		t.Inserir(0, 0, 0, 0);
+		//Confirmar se é um triângulo e qual o seu tipo;		
+	}
+
+}
