6983d4c23266cd1311d8bca7129129569b34a105
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-29 22:51:01 -0300

    Update banco.sql

--- BancoDeDados/2ano/2°bim/escolaIdiomas/banco.sql
+++ BancoDeDados/2ano/2°bim/escolaIdiomas/banco.sql
@@ -2,17 +2,17 @@ CREATE DATABASE bdEscolaIdiomas
 
 CREATE TABLE tbCurso(
 	idCurso INT PRIMARY KEY IDENTITY(1,1)
-	, nomeCurso VARCHAR(45)
+	, nomeCurso VARCHAR(45) NOT NULL
 	, cargaHorariaCurso INT NOT NULL
-	, valorCurso MONEY
+	, valorCurso MONEY NOT NULL
 )
 
 CREATE TABLE tbAluno(
 	idAluno INT PRIMARY KEY IDENTITY(1,1)
 	, nomeAluno VARCHAR(75) NOT NULL
-	, dataNasciAluno SMALLDATETIME
-	, rgAluno VARCHAR(14)
-	, naturalidadeAluno VARCHAR(20)
+	, dataNasciAluno SMALLDATETIME NOT NULL
+	, rgAluno VARCHAR(14) NOT NULL
+	, naturalidadeAluno VARCHAR(20) NOT NULL
 )
 CREATE TABLE tbTurma(
 	idTurma INT PRIMARY KEY IDENTITY(1,1)
@@ -27,3 +27,67 @@ CREATE TABLE tbMatricula(
 	, idAluno INT FOREIGN KEY REFERENCES tbAluno(idAluno)
 	, idTurma INT FOREIGN KEY REFERENCES tbTurma(idTurma)
 )
+
+INSERT INTO tbAluno
+  (nomeAluno, dataNasciAluno, rgAluno, naturalidadeAluno)
+    VALUES
+	 ('Paulo Santos', 10-03-2000, 822821220, 'SP')
+	 ,('Maria da Gloria', 03-10-1999, 822821220, 'SP')
+	 ,('Pedro Noqueira da Silva', 04-04-1998, 822821220, 'SP')
+	 ,('Gilsonn Barros Silva', 09-09-1995, 822821220, 'PE')
+	 ,('Mariana Barbosa Santos', 10-10-2001, 822821220, 'RJ')
+	 ,('Alessandro Pereira', 11-10-2003, 822821220, 'ES')
+	 ,('Aline Melo', 08-10-2001, 822821220, 'RJ')
+
+INSERT INTO tbCurso
+   (nomeCurso, cargaHorariaCurso, valorCurso)
+   VALUES
+    ('Inglês', 2000, 356.00)
+	,('Alemão', 3000, 478.00)
+	,('Espanhol', 4000, 500.00)
+
+INSERT INTO tbTurma
+  (nomeTurma, idCurso, horarioTurma)
+  VALUES
+   ('1|A', 1, '01-01-1900 12:00:00')
+   ,('1|B', 3, '01-01-1900 18:00:00')
+   ,('1|C', 1, '01-01-1900 18:00:00')
+   ,('1|AA', 2, '01-01-1900 19:00:00')
+
+INSERT INTO tbMatricula
+   (dataMatricula, idAluno, idTurma)
+   VALUES
+     (03-10-2015, 1, 1)
+	 ,(04-05-2014, 2, 1)
+	 ,(04-05-2014, 2, 4)
+	 ,(03-05-2012, 3, 2)
+	 ,(03-03-2016, 1, 3)
+	 ,(07-05-2015, 4, 2)
+	 ,(05-07-2015, 4, 3)
+
+	 -- 1 Apresentar os nomes dos alunos ao lado do curso  
+
+	 -- 2 Apresentar a quantidade de alunos matriculados por curso
+
+	 -- 3 Apresentar a quantidade de alunos matriculados por turma
+
+	 -- 4 Apresentar a quantidade de alunos que se matricularam em maio de 2016
+
+	 -- 5 Apresentar o nome dos alunos em ordem alfabética ao lado do nome das turmas e os nomes dos cursos em que estão matriculados
+
+	 -- 6 Apresentar o nome nome dos cursos e os horários em que eles são oferecidos
+
+	 -- 7 Apresentar a quantidade de alunos nascidos por estado que estejam matriculados no curso de inglês
+
+	 -- 8 Apresentar o nome dos alunos ao lado da data de matrícula no formato dd/mm/aaaa
+	 
+	 -- 9 Apresentar os alunos cujo nome comece com A e que estejam matriculados no curso de inglês
+
+	 -- 10 Apresentar a quantidade de matriculas feitas no ano de ano 2016
+
+	 -- 11 Apresentar a quantidade de matriculas por nome do curso
+
+	 -- 12 Apresentar a quantidade de alunos que fazem os cursos que cursos que custam mais de R$ 300,00
+
+	 -- 13 Apresentar os nomes dos alunos que fazem o curso de alemão
+
