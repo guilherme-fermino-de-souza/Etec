c98bceb20813a11d967b16a2d282ac7967ef3700
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-30 11:40:18 -0300

    Create index.php

---
+++ PW2/NewsEtec/30.04.2024/php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="../desc/css/index.css">
+    <title>NewsEtec®</title>
+</head>
+
+<body>
+
+<?php include('navbar.php') ?>
+
+<?php include('cards.php') ?>
+
+<h1>Hello World</h1>
+
+<?php include('footer.php') ?>
+
+
+</body>
+
+</html>
