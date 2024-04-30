bb1b45e485cba5722208211ca23372619c526990
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-04-30 11:39:08 -0300

    Create cards.php

---
+++ PW2/NewsEtec/30.04.2024/php/cards.php
@@ -0,0 +1,96 @@
+
+<!--cards start-->
+    <section id="cards">
+        <div class="container cards">
+            <div class="cards-firstrow">
+                <div class="card">
+                    <div class="img-card">
+                        <img src="../img/galeria/portrait-01-man.webp">
+                    </div>
+                    <div class="text-card">
+                        <h2>Título</h2>
+                        <p>loLorem ipsum dolor sit amet. Et porro natus cum impedit sunt cum aspernatur facilis qui quas
+                            voluptas.</p>
+                    </div>
+                    <div class="button-card">
+                        <button type="button" class="bnt">Contate-me</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img-card">
+                        <img src="../img/galeria/portrait-02-woman.webp">
+                    </div>
+                    <div class="text-card">
+                        <h2>Título</h2>
+                        <p>loLorem ipsum dolor sit amet. Et porro natus cum impedit sunt cum aspernatur facilis qui quas
+                            voluptas.</p>
+                    </div>
+                    <div class="button-card">
+                        <button type="button" class="bnt">Contate-me</button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img-card">
+                        <img src="../img/galeria/portrait-03-woman.webp">
+                    </div>
+                    <div class="text-card">
+                        <h2>Título</h2>
+                        <p>loLorem ipsum dolor sit amet. Et porro natus cum impedit sunt cum aspernatur facilis qui quas
+                            voluptas.</p>
+                    </div>
+                    <div class="button-card">
+                        <button type="button" class="bnt">Contate-me</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="cards-secondrow">
+                <div class="card">
+                    <div class="img-card">
+                        <img src="../img/galeria/portrait-04-man.webp">
+                    </div>
+                    <div class="text-card">
+                        <h2>Título</h2>
+                        <p>loLorem ipsum dolor sit amet. Et porro natus cum impedit sunt cum aspernatur facilis qui quas
+                            voluptas.</p>
+                    </div>
+                    <div class="button-card">
+                        <button type="button" class="bnt">Contate-me</button>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="img-card">
+                        <img src="../img/galeria/portrait-05-woman.webp">
+                    </div>
+                    <div class="text-card">
+                        <h2>Título</h2>
+                        <p>loLorem ipsum dolor sit amet. Et porro natus cum impedit sunt cum aspernatur facilis qui quas
+                            voluptas.</p>
+                    </div>
+                    <div class="button-card">
+                        <button type="button" class="bnt">Contate-me</button>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="img-card">
+                        <img src="../img/galeria/portrait-06-man.webp">
+                    </div>
+                    <div class="text-card">
+                        <h2>Título</h2>
+                        <p>loLorem ipsum dolor sit amet. Et porro natus cum impedit sunt cum aspernatur facilis qui quas
+                            voluptas.</p>
+                    </div>
+                    <div class="button-card">
+                        <button type="button" class="bnt">Contate-me</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--cards end-->
