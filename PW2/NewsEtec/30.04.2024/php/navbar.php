a1fc1bd21e71d3049f82acf0e2c6c92e35e52841
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-05-06 19:02:39 -0300

    Update navbar.php

--- PW2/NewsEtec/30.04.2024/php/navbar.php
+++ PW2/NewsEtec/30.04.2024/php/navbar.php
@@ -1,20 +1,21 @@
-
-    <!--navbar start-->  <!--INÍCIO--> 
-    <header>
-        <nav>
-            <a class="logo" href="../site/index.php">NEWS WALL ETEC</a>
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-            <ul class="nav-list">
-                <li><a href="#conteudo">Início</a></li>
-                <li><a href="#sobre">Sobre</a></li>
-                <li><a href="#cards">Integrantes</a></li>
-                <li><a href="#">Projetos</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
-    </header>
-    <!--navbar end-->
+   <!--navbar start--> <!--INÍCIO-->
+   <header>
+       <nav>
+           <a class="logo" href="../site/index.php">NEWS WALL ETEC</a>
+           <div class="mobile-menu">
+               <div class="line1"></div>
+               <div class="line2"></div>
+               <div class="line3"></div>
+           </div>
+           <ul class="nav-list">
+               <li><a href="#conteudo">Início</a></li>
+               <li><a href="#sobre">Sobre</a></li>
+               <li><a href="#cards">Desenvolvedores</a></li>
+               <li><a href="#">Contato</a></li>
+           </ul>
+           <div class="nav-img">
+               <a href="../login/tela/index.php"><img src="https://cdn3.iconfinder.com/data/icons/cyber-monday-104/32/user_profile_people_member_avatar_account-1024.png"></a>
+           </div>
+       </nav>
+   </header>
+   <!--navbar end-->
