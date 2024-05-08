f39dbc5c2eb345eb9883e842ce5e617e5d80660e
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-05-08 12:10:44 -0300

    Create App.java

---
+++ java/2°ano/2°bim/treino/circulo/App.java
@@ -0,0 +1,21 @@
+package exers100visto2bim;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner in = new Scanner(System.in);
+		
+		areaCirculo a = new areaCirculo();
+		a.Diametro();
+		a.Diametro2(0, 0, 0);
+		
+		a.Diametro3();
+		System.out.println("A area do circulo é "+area);
+		
+	}
+
+}
