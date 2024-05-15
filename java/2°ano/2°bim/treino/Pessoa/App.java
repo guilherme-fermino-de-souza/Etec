112f040e2c34e1f7c703808d6fe9836be8948e0e
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-05-15 15:48:26 -0300

    Update App.java

--- java/2°ano/2°bim/treino/Pessoa/App.java
+++ java/2°ano/2°bim/treino/Pessoa/App.java
@@ -1,20 +1,36 @@
-package Treino;
+package teino;
 
-import java.util,Scanner;
+import java.util.Scanner;
 
-public class Pessoa {
+public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 		Scanner in = new Scanner(System.in);
 		
-		App a = new App();
-		a.CadastrarPessoa1(null);
-		a.CadastrarPessoa2();
+		Pessoa a = new Pessoa();
+		String nome;
+		int idade;
+		double peso;
+		String email;
 		
+		System.out.println("1");
+		System.out.println("Insira o seu nome:");
+		nome = in.nextLine();
+		System.out.println("Insira o seu email:");
+		email = in.nextLine();
+		System.out.println("Insira a sua idade:");
+		idade = in.nextInt();
+		System.out.println("Insira o seu peso:");
+		peso = in.nextDouble();
+		
+		a.CadastrarPessoa1(nome, email, idade, peso);
 		a.ExibirPessoa1();
+		
+		a.CadastrarPessoa2();
 		a.ExibirPessoa2();
+		
+		in.close();
 
 	}
 
