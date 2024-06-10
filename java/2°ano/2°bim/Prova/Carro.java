cca8ac207dec278d19b8a6aba0eb448adf6752ac
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-10 19:22:59 -0300

    Create Carro.java

---
+++ java/2°ano/2°bim/Prova/Carro.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Carro {
+  Scanner in = new Scanner(System.in);
+
+   private String modelo;
+   private String fabricante;
+   private String anoFabricacao;
+
+ public void setModelo(String modelo) {
+   this.modelo = modelo;
+ }
+ public void setFabricante(String fabricante) {
+   this.fabricante = fabricante;
+ }
+ public void setanoFabricacao(String anoFabricacao) {
+   this.anofabricacao = anoFabricacao;
+ }
+
+public String getModelo() {
+  return modelo;
+}
+public String getFabricante() {
+  return fabricante;
+}
+public String getAnoFabricacao() {
+  return anoFabricacao;
+}
