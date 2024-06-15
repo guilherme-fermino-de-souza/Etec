34458746122c8ed08fbf889b03816f1511355970
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-15 20:50:37 -0300

    Update Matematica.java

--- java/2°ano/2°bim/Sobrecarga/Matematica.java
+++ java/2°ano/2°bim/Sobrecarga/Matematica.java
@@ -2,39 +2,41 @@ package sobrecarga;
 import java.util.Scanner;
 public class Matematica {
 	Scanner ler = new Scanner(System.in);
+	private int n1;
+	private int n2;
+	private int r;
 	
-	public void setSoma() {
-		int n1;
-		int n2;
-		int r;
-		
+	public int setSoma (int r) {
+				
 		System.out.print("Insira o 1° número: ");
 		n1 = ler.nextInt();
 		System.out.print("Insira o 2° número: ");
 		n2 = ler.nextInt();
 		r = n1+n2;
-		System.out.println("O resultado da soma é: "+r);
+		return r;
 	}	
-	public void getSoma() {
-		System.out.println("O resultado da soma é: "+this.setSoma());
+	public int getSoma() {
+		return this.setSoma(r);
 	}
 	
-	public int setSoma(int r) {
+	public void setSoma() {
 	    int n1;
 		int n2;
 		System.out.print("Insira o 1° número: ");
 		n1 = ler.nextInt();
 		System.out.print("Insira o 2° número: ");
 		n2 = ler.nextInt();
-		r = n1+n2;
 		
-		return r;
    }
 	public int getSoma(int r) {
-		return this.setSoma(0);
+		this.setSoma(0);
+        r = this.n1+this.n2;
+        return r; 
+        
 	}
    
-   public void setSoma(int n1, int n2, int r) {
+	
+   /*public void setSoma(int n1, int n2, int r) {
 	   	System.out.print("Insira o 1° número: ");
 		n1 = ler.nextInt();
 		System.out.print("Insira o 2° número: ");
@@ -42,8 +44,8 @@ public class Matematica {
 		r = n1+n2;
 		System.out.println("O resultado da soma é: "+r);   
    }  
-   public void getSoma(int r) {
-	   System.out.println(this.setSoma(r, r, r));
-   }
+   public void getSoma() {
+	  return this.setSoma(r, r, r);
+   }*/
 
 }
