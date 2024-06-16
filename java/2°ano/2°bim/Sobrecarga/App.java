0848471eaf2ad947f328d996e26ce72388894ff5
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-16 19:09:28 -0300

    Update App.java

--- java/2°ano/2°bim/Sobrecarga/App.java
+++ java/2°ano/2°bim/Sobrecarga/App.java
@@ -9,10 +9,15 @@ public class App {
 		System.out.println("Os valores da são Soma: "+m.getSoma());
 		
 		System.out.println("Os valores da são Soma: "+m.getSoma(0));
-		/*System.out.print("Insira os valores da Soma: ");
-		m.setSoma();
-		System.out.println("resultado: "+m.setSoma(0));
-		m.setSoma(0, 0, 0);*/
+		
+		System.out.println("Os valores da são Soma: "+m.getSoma(0));
+		
+		System.out.println("Os valores da são Subtração: "+m.getSubtracao());
+		
+		System.out.println("Os valores da são Subtração: "+m.getSubtracao(0));
+	
+		System.out.println("Os valores da são Subtração: "+m.getSubtracao(0));
+
 
 	}
 
