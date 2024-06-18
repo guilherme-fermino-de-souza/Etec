20fc64a68bd66f369237958a20bb72b0c01948f3
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-06-18 16:09:54 -0300

    Update Matematica.java

--- java/2°ano/2°bim/Sobrecarga/Matematica.java
+++ java/2°ano/2°bim/Sobrecarga/Matematica.java
@@ -7,9 +7,9 @@ public class Matematica {
 	private int r;
 	
 	public int setSoma (int r) {			
-		System.out.print("Insira o 1° número da Soma: ");
+		System.out.print("Insira o 1° número da 1° Soma: ");
 		n1 = ler.nextInt();
-		System.out.print("Insira o 2° número da Soma: ");
+		System.out.print("Insira o 2° número da 1° Soma: ");
 		n2 = ler.nextInt();
 		r = n1+n2;
 		return r;
@@ -18,30 +18,28 @@ public class Matematica {
 		return this.setSoma(r);
 	}
 
-	public void setSoma() {
-	    int n1;
-		int n2;
-		System.out.print("Insira o 1° número da Soma: ");
+	public int setSoma(int n1, int n2, int r) {
+		System.out.print("Insira o 1° número da 2° Soma: ");
 		n1 = ler.nextInt();
-		System.out.print("Insira o 2° número da Soma: ");
+		System.out.print("Insira o 2° número da 2° Soma: ");
 		n2 = ler.nextInt();		
+		r = n1+n2;
+		return r;
    }
 	public int getSoma(int r) {
-		this.setSoma(0);
-        r = this.n1+this.n2;
-        return r;       
+		return this.setSoma(0, 0, r);
 	}
    
 	public double setSoma(double n1, double n2, double r) {
-		System.out.print("Insira o 1° número da Soma: ");
+		System.out.print("Insira o 1° número da 3° Soma: ");
 		n1 = ler.nextDouble();
-		System.out.print("Insira o 2° número da Soma: ");
+		System.out.print("Insira o 2° número da 3° Soma: ");
 		n2 = ler.nextDouble();	
-		r= n1 - n1;
+		r = n1+n2;
 		return r;
 	}
-	public double getSoma(double r) {
-		return this.r;
+	public double getSoma(double r, double n1, double n2) {
+		return this.setSoma(n1, n2, r);
 	}
 	
 	
@@ -50,40 +48,38 @@ public class Matematica {
 	
 	public int setSubtracao (int r) {
 		
-		System.out.print("Insira o 1° número da Subtração: ");
+		System.out.print("Insira o 1° número da 1° Subtração: ");
 		n1 = ler.nextInt();
-		System.out.print("Insira o 2° número da Subtração: ");
+		System.out.print("Insira o 2° número da 1° Subtração: ");
 		n2 = ler.nextInt();
-		r = n1 - n2;
+		r = n1-n2;
 		return r;
 	}	
 	public int getSubtracao() {
-		return this.setSoma(r);
+		return this.setSubtracao(r);
 	}
 	
-	public void setSubtracao() {
-	    int n1;
-		int n2;
-		System.out.print("Insira o 1° número da Subtração: ");
+	public int setSubtracao(int n1, int n2, int r) {
+		System.out.print("Insira o 1° número da 2° Subtração: ");
 		n1 = ler.nextInt();
-		System.out.print("Insira o 2° número da Subtração: ");
+		System.out.print("Insira o 2° número da 2° Subtração: ");
 		n2 = ler.nextInt();
-		
+		r = n1-n2;	
+		return r;
    }
 	public int getSubtracao(int r) {
-		this.setSoma(0);
-        r = (this.n1)-(this.n2);
-        return r; 
+		return this.setSubtracao(0, 0, r);
         
 	}
 	public double setSubtracao(double n1, double n2, double r) {
-		System.out.print("Insira o 1° número da Soma: ");
+		System.out.print("Insira o 1° número da 3° Subtração: ");
 		n1 = ler.nextDouble();
-		System.out.print("Insira o 2° número da Soma: ");
+		System.out.print("Insira o 2° número da 3° Subtração: ");
 		n2 = ler.nextDouble();	
+		r = n1-n2;
 		return r;
 	}
-	public double getSubtracao(double r) {
-		return this.r;
+	public double getSubtracao(double n1, double n2, double r) {
+		return this.setSubtracao(n1, n2, r);
 	}
 }
