ccb2f687b366162346457c11db8cc0b6915356a8
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-07-31 17:58:58 -0300

    Create FiguraGeometrica.java

---
+++ java/2°ano/2°semestre/heranca/FiguraGeometrica.java
@@ -0,0 +1,17 @@
+package heranca;
+
+public class FiguraGeometrica {
+	
+	private double altura;
+	private double base;
+	private double r;
+	
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public double getAltura() {
+		return this.altura;
+	}
+
+}
