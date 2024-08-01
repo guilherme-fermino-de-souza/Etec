c620ec36adc3578532869fe1983b5bf9e94aef7e
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-01 18:17:29 -0300

    Update quadrado.java

--- java/2°ano/2°semestre/heranca/quadrado.java
+++ java/2°ano/2°semestre/heranca/quadrado.java
@@ -2,10 +2,9 @@ package heranca;
 
 public class Quadrado extends FiguraGeometrica{
 	FiguraGeometrica f = new FiguraGeometrica();
-	
+	//Feito
 	public void CalcularAreaQuadrado() {
 		r = getBase()*getAltura();
 		System.out.println("A área do Quadrado é: "+r);
 	}
-
 }
