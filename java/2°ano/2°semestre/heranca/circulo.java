49f60555ce53ed5431a5b394a27d0bbb66a9ae35
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-01 18:16:43 -0300

    Update circulo.java

--- java/2°ano/2°semestre/heranca/circulo.java
+++ java/2°ano/2°semestre/heranca/circulo.java
@@ -1,9 +1,9 @@
 package heranca;
 
 public class Circulo extends FiguraGeometrica{
-	
+	//Feito
 	public void CalcularAreaCirculo() {
-		r =(getRaio()*getRaio())*3.14;
+		r = Math.pow(getRaio(), 2)*3.14;
 		System.out.println("A área do Círculo é: "+r);
 	}
 }
