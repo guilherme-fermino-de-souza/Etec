086ea7cd10fda3ba186a94ebc97af55cec79b18d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-07-31 22:14:20 -0300

    Create retangulo.java

---
+++ java/2°ano/2°semestre/heranca/retangulo.java
@@ -0,0 +1,10 @@
+package heranca;
+
+public class Retangulo extends FiguraGeometrica{
+	
+	public void CalcularAreaRetangulo() {
+		r = getAltura()*getBase();
+		System.out.println("A área do Retângulo é: "+r);
+	}
+
+}
