23022bd6bf9974d4c6ee8017e928baf431d8083f
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-01 18:16:13 -0300

    Update FiguraGeometrica.java
    feito

--- java/2°ano/2°semestre/heranca/FiguraGeometrica.java
+++ java/2°ano/2°semestre/heranca/FiguraGeometrica.java
@@ -2,12 +2,16 @@ package heranca;
 import java.util.Scanner;
 public class FiguraGeometrica {
 	Scanner ler = new Scanner(System.in);
-	
+	//Feito
 	private double altura;
 	private double base;
 	private double raio;
+	private double ladoUm;
+	private double ladoDois;
+	private double ladoTres;
 	double r;
 	
+	//ALTURA
 	public double setAltura(double altura) {
 		System.out.println("Insira o valor da Altura: ");
 		altura = ler.nextDouble();
@@ -17,7 +21,7 @@ public class FiguraGeometrica {
 	public double getAltura() {
 		return this.setAltura(altura);
 	}
-	
+	//BASE
 	public double setBase(double base) {
 		System.out.println("Insira o valor da Base: ");
 		base = ler.nextDouble();
@@ -27,7 +31,7 @@ public class FiguraGeometrica {
 	public double getBase() {
 		return this.setBase(base);
 	}
-	
+	//RAIO
 	public double setRaio(double raio) {
 		System.out.println("Insira o valor do Raio: ");
 		raio = ler.nextDouble();
@@ -36,5 +40,35 @@ public class FiguraGeometrica {
 	
 	public double getRaio() {
 		return this.setRaio(raio);
+	}
+	//LADO1
+	public double setLadoUm(double ladoUm) {
+		System.out.println("Insira o valor do 1° lado: ");
+		ladoUm = ler.nextDouble();
+		return ladoUm;
+	}
+	
+	public double getLadoUm() {
+		return this.setLadoUm(ladoUm);
+	}
+	//LADO2
+	public double setLadoDois(double ladoDois) {
+		System.out.println("Insira o valor do 2° lado: ");
+		ladoDois = ler.nextDouble();
+		return ladoDois;
+	}
+	
+	public double getLadoDois() {
+		return this.setLadoDois(ladoDois);
+	}
+	//LADO3
+	public double setLadoTres(double ladoTres) {
+		System.out.println("Insira o valor do 3° lado: ");
+		ladoTres = ler.nextDouble();
+		return ladoTres;
+	}
+	
+	public double getLadoTres() {
+		return this.setLadoTres(ladoTres);
 	}
 }
