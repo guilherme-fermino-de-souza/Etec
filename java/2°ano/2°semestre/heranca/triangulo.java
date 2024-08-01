274f5a3facb8b89f36a6606d2da34bbd5f83b685
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-01 18:18:49 -0300

    Update triangulo.java

--- java/2°ano/2°semestre/heranca/triangulo.java
+++ java/2°ano/2°semestre/heranca/triangulo.java
@@ -1,10 +1,20 @@
 package heranca;
 
 public class Triangulo extends FiguraGeometrica{
-	
+	//Feito
 	public void CalcularAreaTriangulo() {
+		double ladoUm=getLadoUm(), ladoDois=getLadoDois(), ladoTres=getLadoTres();
+		
+		if (ladoUm>ladoDois+ladoTres || ladoDois>ladoUm+ladoTres || ladoTres>ladoUm+ladoDois) {
+			System.out.println("Não é um Triângulo.");
+		} else if (ladoUm==ladoDois && ladoDois==ladoTres) {
+			System.out.println("É um Triângulo Equilátero.");
+		} else if (ladoUm==ladoDois || ladoDois==ladoTres || ladoUm==ladoTres) {
+			System.out.println("É um Triângulo Isósceles.");
+		} else {
+			System.out.println("É um Triângulo Escaleno.");
+		}
 		r = (getAltura()*getBase())/2;
-		System.out.println("A área do Triângulo é: "+r);
+		System.out.println("A área do Triângulo é: "+r+".");
 	}
-
 }
