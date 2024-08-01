fbd51df2f52d88a24ae498c80e04fcadbbb4c58f
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-07-31 22:11:39 -0300

    Update FiguraGeometrica.java

--- java/2°ano/2°semestre/heranca/FiguraGeometrica.java
+++ java/2°ano/2°semestre/heranca/FiguraGeometrica.java
@@ -1,17 +1,40 @@
 package heranca;
-
+import java.util.Scanner;
 public class FiguraGeometrica {
+	Scanner ler = new Scanner(System.in);
 	
 	private double altura;
 	private double base;
-	private double r;
+	private double raio;
+	double r;
 	
-	public void setAltura(double altura) {
-		this.altura = altura;
+	public double setAltura(double altura) {
+		System.out.println("Insira o valor da Altura: ");
+		altura = ler.nextDouble();
+		return altura;
 	}
 	
 	public double getAltura() {
-		return this.altura;
+		return this.setAltura(altura);
 	}
-
+	
+	public double setBase(double base) {
+		System.out.println("Insira o valor da Base: ");
+		base = ler.nextDouble();
+		return base;
+	}
+	
+	public double getBase() {
+		return this.setBase(base);
+	}
+	
+	public double setRaio(double raio) {
+		System.out.println("Insira o valor do Raio: ");
+		raio = ler.nextDouble();
+		return raio;
+	}
+	
+	public double getRaio() {
+		return this.setRaio(raio);
+	}
 }
