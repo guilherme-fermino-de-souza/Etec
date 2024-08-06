f5ab96451b5b2ee339a1982d5e0ee7e2fad408e1
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-05 21:35:43 -0300

    Update bdTransito.sql
    Inserts não inseridos de fato, mas feitos... até certo ponto.

--- BancoDeDados/2ano/3°BIM/bdTransito.sql
+++ BancoDeDados/2ano/3°BIM/bdTransito.sql
@@ -25,3 +25,21 @@ CREATE TABLE tbMultas(
 	,pontosMulta INT NOT NULL
 	,idVeiculo INT FOREIGN KEY REFERENCES tbVeiculo(idVeiculo)
 )
+
+INSERT INTO tbMotorista
+	(nomeMotorista, dataNascimentoMotorista, cpfMotorista, cnhMotorista, pontuacaoMotorista)
+		VALUES
+		('Adriana Noqueira Silva', '11-04-1991', '12345678901', 'A', '2301')
+		,('Diego Moreira Castro', '11-04-1991', '12345678902', 'C', '196')
+		,('Plínio Salgado Marconde', '05-10-2001', '12345678903', 'C', '747')
+
+INSERT INTO tbVeiculo
+	(modeloVeiculo, placaVeiculo, renavamVeiculo, anoVeiculo, idMotorista)
+		VALUES
+		('Coupe Mercedes-Benz Classe C', '123ABCD', '12345678901', '07-10-2014', '1')
+		,('SUV Porshe Macan', '321ABCD', '12345678911', '25-07-2014', '1')
+		,('Compacto Fiat Uno', '426AXYN', '12345678933', '19-01-1983', '2')
+		,('Sedan Chevrolet Onix Plus', '753SDFG', '12345678977', '15-08-2006', '2')
+		,('Desportivo Mazda RX-7', '089TJKC', '12345678909', '30-05-2002', '3')
+		,('Sedan Citroen C4 Lounge', '476EQUV', '21346578910', '01-03-2018', '3')
+-- OBS: falta o da multa! Cuidado!
