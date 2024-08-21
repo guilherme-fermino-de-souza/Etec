f13ee33c7e28d8bb9e37ae6b9dc8b800b19f845e
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-20 23:31:45 -0300

    Update Triangulo.java

--- java/2°ano/2°semestre/heranca2.0/Triangulo.java
+++ java/2°ano/2°semestre/heranca2.0/Triangulo.java
@@ -4,31 +4,31 @@ import java.util.Scanner;
 public class Triangulo extends FiguraGeometrica{
 	//Feito
 	public void CalcularArea() {
-    Scanner ler  = new Scanner(System.in);
-      System.out.println("Insira o Valor do 1º Lado: ");
-      double ladoUm = ler.nextDouble();
-      System.out.println("Insira o Valor do 2º Lado: ");
-      double ladoDois = ler.nextDouble();
-      System.out.println("Insira o Valor do 3º Lado: ");
-		  double ladoTres = ler.nextDouble();
+    	Scanner ler  = new Scanner(System.in);
+      		System.out.println("Insira o Valor do 1º Lado: ");
+      		double ladoUm = ler.nextDouble();
+      		System.out.println("Insira o Valor do 2º Lado: ");
+      		double ladoDois = ler.nextDouble();
+      		System.out.println("Insira o Valor do 3º Lado: ");
+      		double ladoTres = ler.nextDouble();
 		
-		  if (ladoUm>ladoDois+ladoTres || ladoDois>ladoUm+ladoTres || ladoTres>ladoUm+ladoDois) {
-			  System.out.println("Não é um Triângulo.");
-        break();
-        } else {
-		    if (ladoUm==ladoDois && ladoDois==ladoTres) {
-			  System.out.println("É um Triângulo Equilátero.");
-		  } else if (ladoUm==ladoDois || ladoDois==ladoTres || ladoUm==ladoTres) {
-			  System.out.println("É um Triângulo Isósceles.");
-		  } else {
-			  System.out.println("É um Triângulo Escaleno.");
-		  }
+      		if (ladoUm>ladoDois+ladoTres || ladoDois>ladoUm+ladoTres || ladoTres>ladoUm+ladoDois) {
+      			System.out.println("Não é um Triângulo.");
+      		} else {
+      			if (ladoUm==ladoDois && ladoDois==ladoTres) {
+      				System.out.println("É um Triângulo Equilátero.");
+      			} else if (ladoUm==ladoDois || ladoDois==ladoTres || ladoUm==ladoTres) {
+      				System.out.println("É um Triângulo Isósceles.");
+      			} else {
+      				System.out.println("É um Triângulo Escaleno.");
+      			}
 
-        System.out.println("Insira o Valor da Base: ");
-		    double base = ler.nextDouble();
-        System.out.println("Insira o Valor da Altura: ");
-		    double altura = ler.nextDouble();
-		    area = (base*altura)/2;
-		    System.out.println("A área do Triângulo é: "+area+".");
+      			System.out.println("Insira o Valor da Base: ");
+      			double base = ler.nextDouble();
+      			System.out.println("Insira o Valor da Altura: ");
+		    	double altura = ler.nextDouble();
+		    	double area = (base*altura)/2;
+		    	System.out.println("A área do Triângulo é: "+area+".");
+      		}
 	}
 }
