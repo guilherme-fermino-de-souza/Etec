60cc19fce6ecb3545638ac45bff590d0083e3077
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-20 23:06:41 -0300

    Create Triangulo.java

---
+++ java/2°ano/2°semestre/heranca2.0/Triangulo.java
@@ -0,0 +1,34 @@
+package heranca;
+import java.util.Scanner;
+
+public class Triangulo extends FiguraGeometrica{
+	//Feito
+	public void CalcularArea() {
+    Scanner ler  = new Scanner(System.in);
+      System.out.println("Insira o Valor do 1º Lado: ");
+      double ladoUm = ler.nextDouble();
+      System.out.println("Insira o Valor do 2º Lado: ");
+      double ladoDois = ler.nextDouble();
+      System.out.println("Insira o Valor do 3º Lado: ");
+		  double ladoTres = ler.nextDouble();
+		
+		  if (ladoUm>ladoDois+ladoTres || ladoDois>ladoUm+ladoTres || ladoTres>ladoUm+ladoDois) {
+			  System.out.println("Não é um Triângulo.");
+        break();
+        } else {
+		    if (ladoUm==ladoDois && ladoDois==ladoTres) {
+			  System.out.println("É um Triângulo Equilátero.");
+		  } else if (ladoUm==ladoDois || ladoDois==ladoTres || ladoUm==ladoTres) {
+			  System.out.println("É um Triângulo Isósceles.");
+		  } else {
+			  System.out.println("É um Triângulo Escaleno.");
+		  }
+
+        System.out.println("Insira o Valor da Base: ");
+		    double base = ler.nextDouble();
+        System.out.println("Insira o Valor da Altura: ");
+		    double altura = ler.nextDouble();
+		    area = (base*altura)/2;
+		    System.out.println("A área do Triângulo é: "+area+".");
+	}
+}
