38e54403042218388a03a9bae40790c45e5b5dac
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-20 23:29:25 -0300

    Update Circulo.java

--- java/2°ano/2°semestre/heranca2.0/Circulo.java
+++ java/2°ano/2°semestre/heranca2.0/Circulo.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class Circulo extends FiguraGeometrica{
 	//Feito
 	public void CalcularArea() {
-    Scanner ler = new Scanner(System.in);
-		System.out.println("Insira o valor do raio");  
-    double raio = ler.nextDouble(); 
-		r = Math.pow(raio, 2)*3.14;
-		System.out.println("A área do Círculo é: "+r);
+   	 Scanner ler = new Scanner(System.in);
+			System.out.println("Insira o valor do raio");  
+			double raio = ler.nextDouble(); 
+			raio = (raio*raio)*3.14;
+			System.out.println("A área do Círculo é: "+raio);
 	}
 }
