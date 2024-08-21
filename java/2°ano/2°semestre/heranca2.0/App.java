a924dc4679843ef6369f3d0ddee9117bdbc8c4cb
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-20 23:10:38 -0300

    Create App.java

---
+++ java/2°ano/2°semestre/heranca2.0/App.java
@@ -0,0 +1,29 @@
+package heranca;
+import java.util.Scanner;
+public class App {
+	//Feito
+	public static void main(String[] args) {
+		    Scanner ler = new Scanner(System.in);
+		    int escolha;
+				
+		    Quadrado q = new Quadrado();
+		    Circulo c = new Circulo();
+		    Triangulo t = new Triangulo();
+		    Retangulo r = new Retangulo();
+		
+		    System.out.println("Escolha que tipo de conta deseja fazer:");
+		    System.out.println("1.Quadrado 2.Círculo 3.Triângulo 4.Retângulo");
+		    escolha = ler.nextInt();
+		    if (escolha==1) {
+			    q.CalcularArea();
+		    } else if (escolha==2) {
+			    c.CalcularArea();
+		    } else if (escolha==3) {
+			    t.CalcularArea();
+		    } else if (escolha==4) {
+		    	r.CalcularArea();
+		    } else {
+		    	System.out.println("Número inválido");
+		    }
+	    }
+}
