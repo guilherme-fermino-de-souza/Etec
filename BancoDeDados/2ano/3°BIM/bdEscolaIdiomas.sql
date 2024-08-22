4468cfe34f0e328735390103b94165933d0a4f8d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-22 12:14:48 -0300

    Update bdEscolaIdiomas.sql

--- BancoDeDados/2ano/3°BIM/bdEscolaIdiomas.sql
+++ BancoDeDados/2ano/3°BIM/bdEscolaIdiomas.sql
@@ -1,1 +1,18 @@
 CREATE PROCEDURE spBuscaAluno
+
+ @nome VARCHAR(70)
+,@dataNasci SMALLDATETIME
+AS
+	SELECT idAluno, nomeAluno, dataNasciAluno FROM tbAluno
+BEGIN
+	DECLARE @idAluno INT
+	PRINT ('Insira o código de identificação do aluno.')
+	/*IF EXISTS (SELECT idAluno FROM tbAluno)
+	BEGIN 
+		PRINT ('O aluno de código ' + @idAluno + ' se chama ' + @idAluno + ' e nasceu em ' + @dataNasci)
+	END
+	ELSE
+	BEGIN
+		PRINT (Essa identificação é inválida!)
+	END
+END*/
