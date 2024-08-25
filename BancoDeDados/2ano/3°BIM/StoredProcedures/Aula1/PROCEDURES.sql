df07e4d9cfd1356f5df5540653d784f51bef1fae
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-08-24 23:15:41 -0300

    Update PROCEDURES.sql

--- BancoDeDados/2ano/3°BIM/StoredProcedures/Aula1/PROCEDURES.sql
+++ BancoDeDados/2ano/3°BIM/StoredProcedures/Aula1/PROCEDURES.sql
@@ -54,16 +54,16 @@ EXEC spInsereAluninho 'Aurélio','02/08/2009',  'Americano', '12365673912345'
 	
 	CREATE PROCEDURE Exibe_Turma
 	@nomeTurma VARCHAR(20)
-	AS 'Nome Turma'
+	AS 
 	SELECT * FROM tbTurma
 	WHERE @nomeTurma = nomeTurma
 
-	EXEC Exibe_Turma 'Turma A'
+	EXEC Exibe_Turma '1|A'
 
 	SELECT * FROM tbTurma
 -- 5. Criar uma stored procedure Exibe_AlunosdaTurma que, dado o nome da turma exiba os seus alunos. + --
 
-	CREATE PROCEDURE Exibe_AlunodasTurma
+	CREATE PROCEDURE Exibe_AlunodaTurma
 	@nomeTurma VARCHAR(20)
 	AS
 	SELECT nomeTurma, (tbAluno.nomeAluno) FROM tbTurma
@@ -71,7 +71,9 @@ EXEC spInsereAluninho 'Aurélio','02/08/2009',  'Americano', '12365673912345'
 	INNER JOIN tbAluno ON tbMatricula.idAluno = tbAluno.idAluno
 	WHERE @nomeTurma LIKE nomeTurma
 
-	EXEC Exibe_AlunodasTurma 'Turma A'
+	EXEC Exibe_AlunodaTurma '1|A'
+
+	SELECT * FROM tbAluno
 
 /* 6. Criar uma stored procedure para inserir alunos, verificando pelo cpf se o 
 aluno existe ou não, e informar a condição via mensagem. + */
@@ -97,23 +99,33 @@ aluno existe ou não, e informar a condição via mensagem. + */
 				END
 			END
 
-			EXEC spInsereAlunos 'Thomas','02/08/2009', '12345665432', '12345678912345', 'Americano'
+			EXEC spInsereAlunos 'Thomas','02/08/2009', '12345665432', '12345678912345', 'RS'
 
 	SELECT * FROM tbAluno
 
 /* 7. Criar uma stored procedure que rebeba o nome do curso e 
 o nome do aluno e matricule o mesmo no curso pretendido. */
 
-	CREATE PROCEDURE spInsere_Curso
+	CREATE PROCEDURE spInsere_CursosFIVE
 	@nomeAluno VARCHAR(75)
 	,@nomeCurso VARCHAR(45)
+	,@dataMatricula DATETIME
 	AS 
 	BEGIN
 		DECLARE @idAluno INT
-		SELECT nomeCurso, nomeAluno FROM tbAluno
-		INNER JOIN tbMatricula ON tbMatricula.idAluno = tbMatricula.idAluno
-		INNER JOIN tbCurso ON tbAluno.idAluno = tbAluno.idAluno
-		INSERT tbMatricula(dataMatricula, idAluno, idTurma) VALUES (,@idAluno,)
+		SELECT nomeCurso ,nomeAluno  FROM tbAluno
+		INNER JOIN tbMatricula ON tbAluno.idAluno = tbMatricula.idAluno
+		INNER JOIN tbTurma ON tbMatricula.idTurma = tbTurma.idTurma
+		INNER JOIN tbCurso ON tbTurma.idCurso  = tbCurso.idCurso
+		WHERE nomeCurso = @nomeCurso
+		INSERT tbMatricula(dataMatricula, idAluno, idTurma) VALUES (@dataMatricula, @idAluno, @nomeCurso)
+		SET @nomeCurso = (SELECT nomeCurso FROM tbCurso)
+		PRINT('Aluno '+@nomeAluno+' cadastrado no curso com o código '+CONVERT(VARCHAR(5), @nomeCurso) )
+	END
 
-		SELECT * FROM tbAluno
+	EXEC spInsere_CursosFIVE 'denovan', 'Alemão', '2014-05-04'
+
+	SELECT * FROM tbAluno
+	SELECT * FROM tbMatricula
+	SELECT * FROM tbCurso
 		
