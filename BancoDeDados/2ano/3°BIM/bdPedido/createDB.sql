ef9c703f2fd520d81d53a3fcc467c8593bd1c3c4
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-04 17:07:55 -0300

    Create createDB.sql

---
+++ BancoDeDados/2ano/3°BIM/bdPedido/createDB.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE bdPedido
+
+
+CREATE TABLE	tbCliente(
+	idCliente INT PRIMARY KEY IDENTITY(1,1)
+   ,nomeCliente VARCHAR (40) NOT NULL
+   ,cpfCliente CHAR (11) UNIQUE NOT NULL
+   ,ruaCliente VARCHAR(55)
+   ,numCasaCliente VARCHAR(5)
+   ,bairroCliente VARCHAR(20)
+   ,cidadeCliente VARCHAR(25)
+   ,estadoCliente VARCHAR(20)
+   ,cepCliente CHAR(9)
+   ,sexoCliente CHAR (1) NOT NULL
+   ,dataNasCliente SMALLDATETIME 
+)
+
+CREATE TABLE	tbEncomenda(
+	idEncomenda INT PRIMARY KEY IDENTITY(1,1)
+   ,dataEncomenda SMALLDATETIME 
+   ,valorTotalEncomenda MONEY NOT NULL
+   ,dataEntregaEncomenda SMALLDATETIME 
+   ,idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente)
+)
+
+CREATE TABLE	tbCategoriaProduto(
+	idCategoriaProduto INT PRIMARY KEY IDENTITY(1,1)
+	,nomeCategoriaProduto VARCHAR(25)
+)
+
+CREATE TABLE	tbProduto(
+	idProduto INT PRIMARY KEY IDENTITY(1,1)
+	,nomeProduto VARCHAR (35)
+	,precoQuiloProduto MONEY NOT NULL
+	,idCategoriaProduto INT FOREIGN KEY REFERENCES tbCategoriaProduto(idCategoriaProduto)
+)
+
+CREATE TABLE	tbItensEncomenda(
+	idItensEncomenda INT PRIMARY KEY IDENTITY(1,1)
+	,quantidadeQuilos FLOAT
+	,subTotal MONEY NOT NULL
+	,idEncomenda INT FOREIGN KEY REFERENCES tbEncomenda(idEncomenda)
+	,idProduto INT FOREIGN KEY REFERENCES tbProduto(idProduto)
+
+)
