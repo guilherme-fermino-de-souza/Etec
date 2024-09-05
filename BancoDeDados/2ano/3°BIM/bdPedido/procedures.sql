53be5577945385e3f989abc2396ef798d1719cc0
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-04 21:42:12 -0300

    Update procedures.sql

--- BancoDeDados/2ano/3°BIM/bdPedido/procedures.sql
+++ BancoDeDados/2ano/3°BIM/bdPedido/procedures.sql
@@ -57,4 +57,51 @@ EXECUTE spDifNomePro 'Risole Misto', 29, 4
 --DROP PROCEDURE spDifNomeCatPro
 
 
---3.
+--3. Criar uma Stored Procedure para cadastrar os clientes abaixo relacionados, sendo que deverão ser feitas
+--duas validações:			- Verificar pelo CPF se o cliente já existe. Caso já exista emitir a mensagem: 
+--´´Cliente de CPF XXXXX já cadastrado´´ (acrescentar a coluna CPF)			- Verificar se o cliente é mora
+---dor de Itaquera ou Guaianases, pois a confeitaria não realiza entregas para clientes doutros bairros.
+--Caso o cliente não seja morador desses bairros enviar a mensagem ´´Não foi possível cadastrar o cliente
+--XXXX pois o bairro XXXX não é atendido pela confeitaria.´´
+CREATE PROCEDURE spEntreClien
+	@cpfClien CHAR(11)
+	,@nomeClien VARCHAR(40)
+	,@dataNasClien SMALLDATETIME
+	,@ruaClien VARCHAR(55)
+	,@numCasaClien VARCHAR(5)
+	,@cepClien CHAR(9)
+	,@bairroClien VARCHAR(20)
+	,@sexoClien CHAR(1)
+AS 
+BEGIN
+	DECLARE @idClien INT
+	IF EXISTS (SELECT cpfCliente FROM tbCliente WHERE cpfCliente LIKE @cpfClien)
+	BEGIN
+	--Acabo de perceber que o CPF possui 11 números, e não 9. Talvez eu e lembre disso ao passar o código para o GitHub, mas n garanto
+		PRINT('O cpf ' + @cpfClien + ' não pôde ser cadastrado pois já existe: ')
+		SELECT cpfCliente FROM tbCliente
+	END
+	ELSE IF (@bairroClien != 'Itaquera' AND @bairroClien !='Guaianases')
+	BEGIN
+		PRINT('Não foi possível cadastrar o cliente' + @nomeClien +' pois o bairro ' + @bairroClien + ' não é atendido pela confeitaria.')
+	END
+	ELSE
+	BEGIN
+		INSERT tbCliente (cpfCliente, nomeCliente, dataNasCliente, ruaCliente, numCasaCliente, cepCliente, bairroCliente, sexoCliente)
+		VALUES (@cpfClien, @nomeClien, @dataNasClien, @ruaClien, @numCasaClien, @cepClien, @bairroClien, @sexoClien)
+	END
+END
+
+/* EXECUTES CORRETOS
+EXECUTE spEntreClien '012345678', 'Samira Fatah', '05/05/1990', 'Rua Aguapeí', '1000', '08090-000', 'Guaianases', 'F'
+EXECUTE spEntreClien '008642678', 'Celia Nogueira', '06/06/1992', 'Rua Andes', '234', '08456-090', 'Guaianases', 'F'
+EXECUTE spEntreClien '012396318', 'Paulo Cesar Siqueira', '04/04/1984', 'Rua Castelo do Piauí', '232', '08109-000', 'Itaquera', 'M'
+EXECUTE spEntreClien '018503678', 'Rodrigo Favaroni', '09/04/1991', 'Rua Sansão Castelo Branco', '10', '08431-090', 'Guaianases', 'M'
+EXECUTE spEntreClien '012851678', 'Flávia Regina Brito', '22/04/1992', 'Rua Mariano Moro', '300', '08200-123', 'Itaquera', 'F'
+
+EXECUTES ´´INCORRETOS´´
+EXECUTE spEntreClien '012345678', 'awseg', '05/10/1955', 'Rua da Bahêa', '400', '12345-987', 'Guaianases', 'F'
+EXECUTE spEntreClien '012308638', 'awmud', '05/10/1983', 'Beco do Clóvis', '90', '12915-987', 'Cracolândia', 'M'
+*/
+--SELECT idCliente, nomeCliente, dataNasCliente, ruaCliente, numCasaCliente, cepCliente, bairroCliente, sexoCliente FROM tbCliente
+--DROP PROCEDURE spEntreClien	
