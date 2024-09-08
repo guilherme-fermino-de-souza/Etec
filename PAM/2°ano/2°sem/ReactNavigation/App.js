a4c718722e3fc7a86192ef2cff56d661f50c0fb5
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-08 18:19:28 -0300

    Update App.js

--- PAM/2°ano/2°sem/ReactNavigation/App.js
+++ PAM/2°ano/2°sem/ReactNavigation/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from 'react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Correção na importação
 
 /* Como o componente App.js é sempre renderizado
 primeiro, devemos criar rotas nele para os outros 
 componetes */
-
-/* PROJETO: criar uma pasta "src" na raiz do projeto, nesta pasta criar outra chamada "pages",
-que será onde armazenaremos nossas páginas.
-Páginas:   Home
-              index.js
-              style.js
-           Sobre
-              index.js
-              style.js*/
 
 import Home from './src/Pages/Home';
 import Sobre from './src/Pages/Sobre';
