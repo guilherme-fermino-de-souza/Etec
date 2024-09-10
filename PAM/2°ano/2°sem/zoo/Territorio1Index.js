6160b59c21b42a169769d996b6ce773299732d8f
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-10 08:41:03 -0300

    Create Territorio1Index.js

---
+++ PAM/2°ano/2°sem/zoo/Territorio1Index.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import styles from './style'
+
+export default function Territorio1() {
+    return (
+        <View style={styles.container}>
+            <Text>Territorio1 </Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
