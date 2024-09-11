3360b514d8a90fed6e6fe8556517d2348fa4c846
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-11 04:28:50 -0300

    Create App.java

---
+++ java/2°ano/2°semestre/CaixaDialogo/App.java
@@ -0,0 +1,18 @@
+package caixaDialogo;
+
+public class App {
+
+	public static void main(String[] args) {
+		/*
+			1. Valor fixo somado a porcentagem da comissãp sobre o valor 
+		total vendido, o total vendido é calculado pelo valor unitário de cada 
+		unidade vendida.
+			Utilizando caixas de diálogo, crie um programa que calcule o valor total
+		do salário.
+		*/
+		
+		Calculo c = new Calculo();
+		c.CalculoSalario();
+	}
+
+}
