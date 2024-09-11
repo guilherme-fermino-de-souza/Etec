35cdc500938a10f5aedd560370bee891dd21fb1d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-11 05:08:45 -0300

    Update FormTurma.java

--- java/2°ano/2°semestre/FormTurma.java
+++ java/2°ano/2°semestre/FormTurma.java
@@ -51,5 +51,86 @@ public class FormularioTurma extends JFrame{
         setResizable(false); // Não será possível redimensionar o tamanho da janela
         setLocationRelativeTo(null); // Centraliza a janela na tela
         setVisible(true); // SEMPRE A ÚLTIMA LINHA (False por padrão)
+	    /* Seu código
+     import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+public class FormularioTurma extends JFrame {
+
+
+	
+	private static final long serialVersionUID = 1L;
+	private JLabel lbNome;
+	private JTextField txNome;
+	private JButton btEnviar;
+	private JTextField txResult;
+	private JLabel lbResult;
+
+	public FormularioTurma() {
+
+		setTitle("Calculadora-protótipo");
+		setSize(900, 700); // largura e altura
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(null);
+		setResizable(false);
+		setLocationRelativeTo(null);
+
+		lbNome = new JLabel();
+		lbNome.setText("Primeiro valor: ");
+		lbNome.setBounds(100, 100, 200, 80); //posição x, posição y, largura e altura
+		add(lbNome);
+
+		txNome = new JTextField();
+		txNome.setBounds(220, 120, 140, 45); //posição x, posição y, largura e altura
+		add(txNome);
+
+		btEnviar = new JButton();
+		btEnviar.setBounds(205, 200, 100, 30); //posição x, posição y, largura e altura
+		btEnviar.setText("Somar");
+		add(btEnviar);
+
+		btEnviar = new JButton();
+		btEnviar.setBounds(305, 200, 100, 30); //posição x, posição y, largura e altura
+		btEnviar.setText("Subtrair");
+		add(btEnviar);
+
+
+
+		lbNome = new JLabel();
+		lbNome.setText("Segundo valor: ");
+		lbNome.setBounds(500, 100, 200, 80); //posição x, posição y, largura e altura
+		add(lbNome);
+
+		txNome = new JTextField();
+		txNome.setBounds(600, 120, 140, 45); //posição x, posição y, largura e altura
+		add(txNome);
+
+		btEnviar = new JButton();
+		btEnviar.setBounds(405, 200, 100, 30); //posição x, posição y, largura e altura
+		btEnviar.setText("Multiplicar");
+		add(btEnviar);
+
+		btEnviar = new JButton();
+		btEnviar.setBounds(505, 200, 100, 30); //posição x, posição y, largura e altura
+		btEnviar.setText("Dividir");
+		add(btEnviar);
+
+		lbResult = new JLabel();
+		lbResult.setText("Resultado: ");
+		lbNome.setBounds(350, 300, 200, 80); //posição x, posição y, largura e altura
+		add(lbResult);
+
+		txResult = new JTextField();
+		txResult.setBounds(350, 350, 140, 45); //posição x, posição y, largura e altura
+		add(txResult);
+
+
+		setVisible(true);
+	}
+}
+     */
     }
 }
