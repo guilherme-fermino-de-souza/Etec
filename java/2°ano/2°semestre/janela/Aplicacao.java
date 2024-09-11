7c67469ab3c687369b1f6f0b58d46a9d5dc7bb92
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-11 11:28:46 -0300

    Update Aplicacao.java

--- java/2°ano/2°semestre/janela/Aplicacao.java
+++ java/2°ano/2°semestre/janela/Aplicacao.java
@@ -1,1 +1,9 @@
-INSIRA TEXTO...
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculadora c = new Calculadora();
+		c.Calcular();
+	}
+
+}
