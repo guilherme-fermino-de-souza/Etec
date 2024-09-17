cb1dfe00d24234e613dcebea5ba2c55d71043962
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-17 08:20:09 -0300

    Update LoginStyle.js

--- PAM/2°ano/2°sem/zoo/LoginStyle.js
+++ PAM/2°ano/2°sem/zoo/LoginStyle.js
@@ -1,1 +1,75 @@
-tete
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    maxWidth: 300,
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+
+  header: {
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderTopWidth: 5,
+    borderBottomWidth: 5,
+    backgroundColor: '#3e3e3e'
+  },
+
+  headerTwo: {
+    flexDirection: 'row',
+    margin: 25,
+  },
+
+  title: {
+    fontSize: 28,
+    marginLeft: 10,
+    marginTop: 7,
+    color: 'white',
+  },
+
+  formContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderTopWidth: 5,
+    borderBottomWidth: 5,
+    padding: 10,
+  },
+
+  formTitle: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+
+  infoContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderTopWidth: 5,
+    borderBottomWidth: 5,
+    padding: 10,
+  },
+
+  answerContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderTopWidth: 5,
+    borderBottomWidth: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+
+  tableTitle: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+});
