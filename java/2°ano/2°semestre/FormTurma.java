b06276c49b735b78a7568728aef04852ae4179b2
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-18 11:22:17 -0300

    Update FormTurma.java

--- java/2°ano/2°semestre/FormTurma.java
+++ java/2°ano/2°semestre/FormTurma.java
@@ -1,5 +1,83 @@
-package janelas;
+package formTurma;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+
+public class FormularioTurma extends JFrame {
+
+
+	private static final long serialVersionUID = 1L;
+	private JLabel lbNome;
+	private JTextField txNome;
+	private JTextField txCpf;
+	private JButton btEnviar;
+	private JTextField txResult;
+	private JLabel lbResult;
+
+	public FormularioTurma() {
+
+		setTitle("Formulário-protótipo");
+		setSize(900, 700); // largura e altura
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(null);
+		setResizable(false);
+		setLocationRelativeTo(null);
+
+		lbNome = new JLabel();
+		lbNome.setText("Nome do Aluno: ");
+		lbNome.setBounds(100, 100, 200, 80); //posição x, posição y, largura e altura
+		add(lbNome);
+
+		txNome = new JTextField();
+		txNome.setBounds(220, 120, 140, 45); //posição x, posição y, largura e altura
+		add(txNome);
+
+
+		lbNome = new JLabel();
+		lbNome.setText("CPF do Aluno: ");
+		lbNome.setBounds(500, 100, 200, 80); //posição x, posição y, largura e altura
+		add(lbNome);
+
+		txNome = new JTextField();
+		txNome.setBounds(600, 120, 140, 45); //posição x, posição y, largura e altura
+		add(txCpf);
+
+		btEnviar = new JButton();
+		btEnviar.setBounds(505, 200, 100, 30); //posição x, posição y, largura e altura
+		btEnviar.setText("Verificar");
+		btEnviar.addActionListener(new ActionListener() {
+        	@Override
+        	public void actionPerformed(ActionEvent e) {
+        		String  nome = (txNome.getText());
+        		String cpf = (txCpf.getText());
+        		JOptionPane.showMessageDialog(null, "O nome do aluno é: "+nome+" e este é seu CPF"+cpf);
+        	}
+        });
+        
+
+		lbResult = new JLabel();
+		lbResult.setText("identificação do Aluno: ");
+		lbNome.setBounds(350, 300, 200, 80); //posição x, posição y, largura e altura
+		add(lbResult);
+
+		txResult = new JTextField();
+		txResult.setBounds(350, 350, 140, 45); //posição x, posição y, largura e altura
+		add(txResult);
+
+
+		setVisible(true);
+	}
+}
+     
+
+/* 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,9 +86,7 @@ import java.util.Scanner;
 
 public class FormularioTurma extends JFrame{
 
-    /**
-	 * 
-	 */
+    
 	private static final long serialVersionUID = 1L;
 	// Atributo de instância 
 	
@@ -23,7 +99,7 @@ public class FormularioTurma extends JFrame{
     	Scanner ler = new Scanner(System.in);
         // Número 1
         lbNumero1 = new JLabel(); // Inicializando o JLabel(TEXTO)
-        lbNumero1.setText("Primeiro Número: ");
+        lbNumero1.setText("Nome do Aluno: ");
         lbNumero1.setBounds(50, 100, 175, 35); // Posição x(horizontal),y(vertical) e largura e altura
         add(lbNumero1);
 
@@ -33,7 +109,7 @@ public class FormularioTurma extends JFrame{
         
         // Número 2
         lbNumero2 = new JLabel(); // Inicializando o JLabel(TEXTO)
-        lbNumero2.setText("Segundo Número: ");
+        lbNumero2.setText("Turma do Aluno: ");
         lbNumero2.setBounds(50, 150, 175, 35); // Posição x(horizontal),y(vertical) e largura e altura
         add(lbNumero2);
         
@@ -51,85 +127,4 @@ public class FormularioTurma extends JFrame{
         setResizable(false); // Não será possível redimensionar o tamanho da janela
         setLocationRelativeTo(null); // Centraliza a janela na tela
         setVisible(true); // SEMPRE A ÚLTIMA LINHA (False por padrão)
-	    /* Seu código
-    import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-
-
-public class FormularioTurma extends JFrame {
-
-
-	private static final long serialVersionUID = 1L;
-	private JLabel lbNome;
-	private JTextField txNome;
-	private JButton btEnviar;
-	private JTextField txResult;
-	private JLabel lbResult;
-
-	public FormularioTurma() {
-
-		setTitle("Calculadora-protótipo");
-		setSize(900, 700); // largura e altura
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(null);
-		setResizable(false);
-		setLocationRelativeTo(null);
-
-		lbNome = new JLabel();
-		lbNome.setText("Primeiro valor: ");
-		lbNome.setBounds(100, 100, 200, 80); //posição x, posição y, largura e altura
-		add(lbNome);
-
-		txNome = new JTextField();
-		txNome.setBounds(220, 120, 140, 45); //posição x, posição y, largura e altura
-		add(txNome);
-
-		btEnviar = new JButton();
-		btEnviar.setBounds(205, 200, 100, 30); //posição x, posição y, largura e altura
-		btEnviar.setText("Somar");
-		add(btEnviar);
-
-		btEnviar = new JButton();
-		btEnviar.setBounds(305, 200, 100, 30); //posição x, posição y, largura e altura
-		btEnviar.setText("Subtrair");
-		add(btEnviar);
-
-
-
-		lbNome = new JLabel();
-		lbNome.setText("Segundo valor: ");
-		lbNome.setBounds(500, 100, 200, 80); //posição x, posição y, largura e altura
-		add(lbNome);
-
-		txNome = new JTextField();
-		txNome.setBounds(600, 120, 140, 45); //posição x, posição y, largura e altura
-		add(txNome);
-
-		btEnviar = new JButton();
-		btEnviar.setBounds(405, 200, 100, 30); //posição x, posição y, largura e altura
-		btEnviar.setText("Multiplicar");
-		add(btEnviar);
-
-		btEnviar = new JButton();
-		btEnviar.setBounds(505, 200, 100, 30); //posição x, posição y, largura e altura
-		btEnviar.setText("Dividir");
-		add(btEnviar);
-
-		lbResult = new JLabel();
-		lbResult.setText("Resultado: ");
-		lbNome.setBounds(350, 300, 200, 80); //posição x, posição y, largura e altura
-		add(lbResult);
-
-		txResult = new JTextField();
-		txResult.setBounds(350, 350, 140, 45); //posição x, posição y, largura e altura
-		add(txResult);
-
-
-		setVisible(true);
-	}
-}
-     */
-    }
-}
+	   */ //Seu código
