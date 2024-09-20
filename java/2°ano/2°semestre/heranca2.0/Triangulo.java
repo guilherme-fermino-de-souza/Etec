4b242d31eb1e98fffef581c991525d5ba97ccb42
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-20 02:09:44 -0300

    Update Triangulo.java

--- java/2°ano/2°semestre/heranca2.0/Triangulo.java
+++ java/2°ano/2°semestre/heranca2.0/Triangulo.java
@@ -1,32 +1,54 @@
-package heranca;
+package figuras;
 import java.util.Scanner;
 
 public class Triangulo extends FiguraGeometrica{
 	//Feito
+	double ladoUm,ladoDois, ladoTres;
 	public boolean eUmTriangulo() {
 		Scanner ler  = new Scanner(System.in);
 		System.out.println("Insira o Valor do 1º Lado: ");
-		double ladoUm = ler.nextDouble();
+		ladoUm = ler.nextDouble();
 		System.out.println("Insira o Valor do 2º Lado: ");
-		double ladoDois = ler.nextDouble();
+		ladoDois = ler.nextDouble();
 		System.out.println("Insira o Valor do 3º Lado: ");
-		double ladoTres = ler.nextDouble();
+		ladoTres = ler.nextDouble();
 		if (ladoUm>ladoDois+ladoTres || ladoDois>ladoUm+ladoTres || ladoTres>ladoUm+ladoDois) {
 			System.out.println("Não é um Triângulo.");
 			return false;
 		} else {
 			System.out.println("É um Triângulo.");	
 			return true;
-		}
+		} 
+		
 	}
 
 	public void CalcularArea() {
 		Scanner ler = new Scanner(System.in);
-		System.out.println("Insira o Valor da Base: ");
-		double base = ler.nextDouble();
-		System.out.println("Insira o Valor da Altura: ");
-		double altura = ler.nextDouble();
-		double area = (base*altura)/2;
-		System.out.println("A área do Triângulo é: "+area+".");
+		if (ladoUm == ladoDois && ladoDois == ladoTres) { //Equilatero
+				System.out.println("O Triângulo é Equilatero");
+				this.area = ((lado*lado) * Math.sqrt(3))/4;
+		} else if (ladoUm != ladoDois && ladoDois != ladoTres) { //Escaleno
+				System.out.println("O Triângulo é Escaleno");
+			
+		} else { //Isósceles
+				System.out.println("O Triângulo é Isósceles");
+				if (ladoUm <= ladoDois && ladoDois == ladoTres) {
+					double hDois = ((ladoUm/2)*(ladoUm/2)) - (ladoDois*ladoDois);
+					this.altura = Math.sqrt(hDois);
+					this.lado = ladoUm;
+				} else if (ladoTres <= ladoUm && ladoUm == ladoDois) {
+					double hDois = ((ladoTres/2)*(ladoTres/2)) - (ladoDois*ladoDois);
+					this.altura = Math.sqrt(hDois);
+					this.lado = ladoTres;
+				} else {
+					double hDois = ((ladoDois/2)*(ladoDois/2)) - (ladoTres*ladoTres);
+					this.altura = Math.sqrt(hDois);
+					this.lado = ladoDois;
+				}
+				this.area = (lado*altura)/2;
+				
+		    }
+		
+		System.out.println("A área do Triângulo é: "+this.area+" e a Altura é "+this.altura+".");
 	}
 }
