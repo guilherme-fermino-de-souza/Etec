46b5ab0d01c6098864ebf1f7ee5806e4688b74f8
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-09-20 01:17:49 -0300

    Update Quadrado.java

--- java/2°ano/2°semestre/heranca2.0/Quadrado.java
+++ java/2°ano/2°semestre/heranca2.0/Quadrado.java
@@ -1,4 +1,4 @@
-package heranca;
+package figuras;
 import java.util.Scanner;
 
 public class Quadrado extends FiguraGeometrica{
@@ -6,8 +6,8 @@ public class Quadrado extends FiguraGeometrica{
 	public void CalcularArea() {
     	Scanner ler = new Scanner(System.in);
     		System.out.println("Insira o valor do Lado: ");
-    		double lado = ler.nextDouble();
-    		double area = lado*lado;
+    		this.lado = ler.nextDouble();
+    		this.area = lado*lado;
     		System.out.println("A área do Quadrado é: "+area);
 	}
 }
