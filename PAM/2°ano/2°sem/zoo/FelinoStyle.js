6dcba3ddccfcc3cd1f20422302f41fa6407d4d86
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-01 02:01:35 -0300

    Create FelinoStyle.js

---
+++ PAM/2°ano/2°sem/zoo/FelinoStyle.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#e8f0fe',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonCont: {
+      marginBottom: 20,
+    },
+    button: {
+      fontSize: 80,
+      color:'#FFE74A',
+    },
+    modal: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#e8f0fe',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 30,
+      color: '#0C0C0D',
+    },
+    
+    description: {
+      fontSize: 10,
+      color: 'black',
+      fontWeight: 500,
+      marginBottom: 15,
+      textAlign: 'left',
+      width: '80%'
+    },
+    imagemContainer: {
+      width: '80%',
+      height: '20%',
+      marginBottom: 15,
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+      margin: 5,
+    },
+  });
