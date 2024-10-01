23c1ef06eca33b1ea7c48555e747332c15d5993b
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-01 02:04:39 -0300

    Update HomeIndex.js

--- PAM/2°ano/2°sem/zoo/HomeIndex.js
+++ PAM/2°ano/2°sem/zoo/HomeIndex.js
@@ -1,22 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View, Button } from "react-native-web";
-import styles from './style'; // Link estilo
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 import { useNavigation } from '@react-navigation/native';
-// Dá acesso a navegação de objeto
+import styles from './style';
 
-export default function Home() { // Nome da página
-    const navigation = useNavigation(); // Const para a navegação
-    
-    return (
-        <View style={styles.container}>
-            <Text>Página de home </Text>
-            <Button title="Ter1" onPress={ () => navigation.navigate('Ter1')}/>
-            <Button title="Ter2" onPress={ () => navigation.navigate('Ter2')}/>
-            <Button title="Ter3" onPress={ () => navigation.navigate('Ter3')}/>
-            <Button title="Ter4" onPress={ () => navigation.navigate('Ter4')}/>
-            <Button title="Ter5" onPress={ () => navigation.navigate('Ter5')}/>
-            <Button title="Ter6" onPress={ () => navigation.navigate('Ter6')}/>
-            <StatusBar style="auto" />
-        </View>
-    )
+export default function Home() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={(styles.container)} >
+      <StatusBar style="auto" />
+        <Text style={styles.texto}>Bem Vindo Ao Zoológico</Text>
+        <Button
+        title="Fazer login"
+        onPress={ () => navigation.navigate('Login')}/>
+    </View>
+  );
 }
