2b2170233bf5c69eb1deec806fe9f82e30cfdb26
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-01 00:31:06 -0300

    Create TerritorioStyle.js

---
+++ PAM/2°ano/2°sem/zoo/TerritorioStyle.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  territorioArea:{
+    flex:1,
+    flexDirection:'row',
+    height:'100%',
+    width:'85%',
+    margin:10,
+  },
+  buttonArea:{
+    width:'50%',
+    height:'100%',
+    marginLeft:'0.5%',
+    marginRight:'1.5%'
+  },
+  territorio:{
+    width:'100%',
+    height:'100%',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  header:{
+    flex:0.5
+  }
+
+});
