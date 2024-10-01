c7f3e2483c2d3575988578381ec6bb985108f986
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-01 02:05:09 -0300

    Update HomeStyle.js

--- PAM/2°ano/2°sem/zoo/HomeStyle.js
+++ PAM/2°ano/2°sem/zoo/HomeStyle.js
@@ -1,9 +1,16 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-        container:{
-            flex:1,
-            alignItems:"center",
-            justifyContent:"center",
-        },
-})
+    container: {
+      flex: 1,
+      backgroundColor: '#828282',
+      justifyContent:'center',
+      alignItens:'center'
+    },
+    texto:{
+      alignSelf:'center',
+      marginBottom:'25%',
+      fontSize:30
+    }
+  
+  });
