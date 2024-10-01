dd65b9161e40947a950899dd4804db982dc0e6d7
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-01 08:34:16 -0300

    Update HomeIndex.js

--- PAM/2°ano/2°sem/zoo/HomeIndex.js
+++ PAM/2°ano/2°sem/zoo/HomeIndex.js
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { Button } from 'react-native-web';
 import { useNavigation } from '@react-navigation/native';
 import styles from './style';
 
-export default function Home() {
+export default function Home() { // Tela Principal
   const navigation = useNavigation();
 
   return (
     <View style={(styles.container)} >
-      <StatusBar style="auto" />
-        <Text style={styles.texto}>Bem Vindo Ao Zoológico</Text>
-        <Button
-        title="Fazer login"
-        onPress={ () => navigation.navigate('Login')}/>
+      <View style={(styles.home)} >
+        <StatusBar style="auto" />
+        <Text style={styles.texto}>Zoológico</Text>
+          <TouchableOpacity style={styles.button}
+            title="Fazer login"
+            onPress={ () => navigation.navigate('Login')}>
+            <Text style={styles.buttontext}>Entrar</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   );
 }
