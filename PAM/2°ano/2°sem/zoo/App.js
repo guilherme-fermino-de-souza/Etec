5f484e5922985973d2a5f6f53664c5ab760be667
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-01 02:02:34 -0300

    Update App.js

--- PAM/2°ano/2°sem/zoo/App.js
+++ PAM/2°ano/2°sem/zoo/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Correção na importação
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 /* Como o componente App.js é sempre renderizado
 primeiro, devemos criar rotas nele para os outros 
@@ -17,14 +17,9 @@ Páginas:   Home
 
 
 import Home from './src/Pages/Home';
-import Sobre from './src/Pages/Sobre';
-import Sobre from './src/Pages/Login';
-import Sobre from './src/Pages/Territorio1';
-import Sobre from './src/Pages/Territorio2';
-import Sobre from './src/Pages/Territorio3';
-import Sobre from './src/Pages/Territorio4';
-import Sobre from './src/Pages/Territorio5';
-import Sobre from './src/Pages/Territorio6';
+import Login from './src/Pages/Login';
+import Territorios from './src/Pages/Territorios';
+import Felinos from './src/Pages/Felinos';
 
 const Stack = createNativeStackNavigator ();
 
@@ -32,17 +27,14 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}
-          options={{ // Configuração do ccabeçalho
-            title: "Tela Inicial",
-            headerStyle:{backgroundColor: "#121212"},
-            hearderTintColor:"#FFa500",
-            //headerShow: false,
-            /* Deve ser criada para cada página no arquivo
-            que foi criado a rota */
-          }}
-        />
-        <Stack.Screen name="Ter1" component={Ter1}/>
+      <Stack.Screen name="Home" component={Home}
+        options={{headerStyle:{backgroundColor:'#545454'},headerTintColor:'white'}}/>
+      <Stack.Screen name="Login" component={Login}
+        options={{headerStyle:{backgroundColor:'#545454'},headerTintColor:'white'}}/>
+      <Stack.Screen name="Territorios" component={Territorios}
+        options={{headerStyle:{backgroundColor:'#545454'},headerTintColor:'white'}}/>
+      <Stack.Screen name="Felinos" component={Felinos}
+        options={{headerStyle:{backgroundColor:'#545454'},headerTintColor:'white'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
