c87ac8c8bb9d4d4339d684a799c1a8cbad4860ea
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-05 20:06:49 -0300

    Update TerritorioIndex.js

--- PAM/2°ano/2°sem/zoo/TerritorioIndex.js
+++ PAM/2°ano/2°sem/zoo/TerritorioIndex.js
@@ -11,9 +11,53 @@ export default function Territorios() {
       <View style={styles.territorioArea}>
         <View style={styles.buttonArea}>
             <Pressable style={styles.territorio} onPress={ () => navigation.navigate('Felinos')}>
-              <Image style={{width:'100%',height:'30%'}}
-              source={{uri:'https://w7.pngwing.com/pngs/581/763/png-transparent-white-tiger-drawing-tiger-white-mammal-animals.png'}} />
+              <Image style={{width:'100%',height:'100%'}}
+              source={{uri:'https://sun9-6.userapi.com/s/v1/ig2/UKJnOv0x_UyjSkVdi0zzb-UhTaydpCVM9ZlanspTnLPWSC-4MJxQ1jmEECAKOHsEJ4Zd1_greQEDFjE4UGQRpAAU.jpg?quality=95&as=32x31,48x47,72x71,108x106,160x157,240x235,360x353,480x470,540x529,640x627,720x705,828x811&from=bu&u=Qw1ThUEzJsOEGFnQ3Q57xBe3bmLkjzRyyzKCaxdcyvw&cs=828x811'}} />
               <Text style={styles.text}>Felinos</Text>
+            </Pressable>
+        </View>
+        
+        <View style={styles.buttonArea}>
+            <Pressable style={styles.territorio} onPress={ () => navigation.navigate('Felinos')}>
+              <Image style={{width:'100%',height:'100%'}}
+              source={{uri:'https://avatars.mds.yandex.net/i?id=c6923e551ea263a4eba7a603fca24bce-5663061-images-thumbs&n=13'}} />
+              <Text style={styles.text}>Canideos</Text>
+            </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.territorioArea}>
+        <View style={styles.buttonArea}>
+            <Pressable style={styles.territorio} onPress={ () => navigation.navigate('Felinos')}>
+              <Image style={{width:'100%',height:'100%'}}
+              source={{uri:'https://sun9-6.userapi.com/s/v1/ig2/UKJnOv0x_UyjSkVdi0zzb-UhTaydpCVM9ZlanspTnLPWSC-4MJxQ1jmEECAKOHsEJ4Zd1_greQEDFjE4UGQRpAAU.jpg?quality=95&as=32x31,48x47,72x71,108x106,160x157,240x235,360x353,480x470,540x529,640x627,720x705,828x811&from=bu&u=Qw1ThUEzJsOEGFnQ3Q57xBe3bmLkjzRyyzKCaxdcyvw&cs=828x811'}} />
+              <Text style={styles.text}>Felinos</Text>
+            </Pressable>
+        </View>
+        
+        <View style={styles.buttonArea}>
+            <Pressable style={styles.territorio} onPress={ () => navigation.navigate('Felinos')}>
+              <Image style={{width:'100%',height:'100%'}}
+              source={{uri:'https://avatars.mds.yandex.net/i?id=c6923e551ea263a4eba7a603fca24bce-5663061-images-thumbs&n=13'}} />
+              <Text style={styles.text}>Canideos</Text>
+            </Pressable>
+        </View>
+      </View>
+
+      <View style={styles.territorioArea}>
+        <View style={styles.buttonArea}>
+            <Pressable style={styles.territorio} onPress={ () => navigation.navigate('Felinos')}>
+              <Image style={{width:'100%',height:'100%'}}
+              source={{uri:'https://sun9-6.userapi.com/s/v1/ig2/UKJnOv0x_UyjSkVdi0zzb-UhTaydpCVM9ZlanspTnLPWSC-4MJxQ1jmEECAKOHsEJ4Zd1_greQEDFjE4UGQRpAAU.jpg?quality=95&as=32x31,48x47,72x71,108x106,160x157,240x235,360x353,480x470,540x529,640x627,720x705,828x811&from=bu&u=Qw1ThUEzJsOEGFnQ3Q57xBe3bmLkjzRyyzKCaxdcyvw&cs=828x811'}} />
+              <Text style={styles.text}>Felinos</Text>
+            </Pressable>
+        </View>
+        
+        <View style={styles.buttonArea}>
+            <Pressable style={styles.territorio} onPress={ () => navigation.navigate('Felinos')}>
+              <Image style={{width:'100%',height:'100%'}}
+              source={{uri:'https://avatars.mds.yandex.net/i?id=c6923e551ea263a4eba7a603fca24bce-5663061-images-thumbs&n=13'}} />
+              <Text style={styles.text}>Canideos</Text>
             </Pressable>
         </View>
       </View>
