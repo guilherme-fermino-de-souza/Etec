039e0915af5d299ebe1e37695eba7d68c5121218
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-08 17:21:06 -0300

    Update triggers.sql
    Incompleto

--- BancoDeDados/2ano/3°BIM/bdBanquinho/triggers.sql
+++ BancoDeDados/2ano/3°BIM/bdBanquinho/triggers.sql
@@ -1,9 +1,57 @@
-/*2 - Criar dois triggers que:
-A) Ao ser feito um depósito atualize o saldo da conta corrente, somando à quantia depositada*/
+/*2 - Criar dois triggers que:			''      DATE '1990-02-10'     smalldatetime '08-02-2016 23:21:00'
 
+A) Ao ser feito um depósito atualize o saldo da conta corrente, somando à quantia depositada*/
+CREATE TRIGGER tgAtualizaSaldoConCor
+ON tbDeposito
+AFTER INSERT
+AS
+
+	 DECLARE @idNumConta INT, @idDeposito INT, @valDeposito INT
+ 
+	 SET @idNumConta = (SELECT idNumConta FROM tbDeposito WHERE idNumConta = @idNumConta)
+
+	 SELECT @idDeposito = idDeposito
+ 
+	 SELECT @valDeposito= valorDeposito
+ 
+	 UPDATE tbContaCorrente
+		SET saldoConta = saldoConta - @valDeposito
+		 WHERE idNumConta = @idNumConta
+
+
+	-- Testando Trigger
+SELECT * FROM tbContaCorrente
+SELECT * FROM tbDeposito
+
+INSERT INTO tbSaque (valorSaque, dataSaque, horaSaque, idNumConta)
+	VALUES (GETDATE(), 5, 1)
 
 
 
 
 
 --B) Ao ser feito um saque atualize o saldo da conta corrente, descontando o valor da última (caso haja saldo bastante)
+CREATE TRIGGER tgAtualizaSaldoContaCorrente 
+ON tbSaque
+AFTER INSERT
+AS
+
+	 DECLARE @idNumConta INT, @idSaque INT, @valSaque INT
+ 
+	 SET @idNumConta = (SELECT idNumConta FROM tbSaque WHERE idNumConta = @idNumConta)
+
+	 SELECT @idSaque = idSaque
+ 
+	 SELECT @valSaque = valorSaque
+ 
+	 UPDATE tbContaCorrente
+		SET saldoConta = saldoConta - @valSaque
+		 WHERE idNumConta = @idNumConta
+
+
+	-- Testando Trigger
+SELECT * FROM tbContaCorrente
+SELECT * FROM tbSaque
+
+INSERT INTO tbSaque (valorSaque, dataSaque, horaSaque, idNumConta)
+	VALUES(55, '15-12-2024', '15-12-2024 17:49:00', 1)
