aaee1ae2d9af3deee3d04fc1717b7d1885f2a8ba
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-08 17:15:36 -0300

    Update inserts.sql

--- BancoDeDados/2ano/3°BIM/bdBanquinho/inserts.sql
+++ BancoDeDados/2ano/3°BIM/bdBanquinho/inserts.sql
@@ -10,3 +10,6 @@ INSERT INTO tbDeposito (valorDeposito, dataDeposito, horaDeposito, idNumConta)
 	VALUES(150, '08-12-2024', '08-12-2024 13:21:00', 1)
 	,(2.000, '13-12-2024', '13-12-2024 20:57:00', 2)
 
+INSERT INTO tbSaque (valorSaque, dataSaque, horaSaque, idNumConta)
+	VALUES(50, '15-12-2024', '15-12-2024 17:49:00', 1)
+	,(600, '23-12-2024', '23-12-2024 11:26:00', 2)
