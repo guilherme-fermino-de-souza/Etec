a9e1149c00e6750d1e4734a84a82843b513c5389
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-08 16:46:41 -0300

    Update creates.sql

--- BancoDeDados/2ano/3°BIM/bdBanquinho/creates.sql
+++ BancoDeDados/2ano/3°BIM/bdBanquinho/creates.sql
@@ -10,13 +10,13 @@ CREATE TABLE tbCliente(
 CREATE TABLE tbContaCorrente(
 	idNumConta INT PRIMARY KEY IDENTITY(1, 1)
 	,numConta INT
-	,saldoConta INT
+	,saldoConta MONEY
 	,idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente)
 )	
 
 CREATE TABLE tbDeposito(
 	idDeposito INT PRIMARY KEY IDENTITY(1, 1)
-	,valorDeposito VARCHAR(15)
+	,valorDeposito MONEY
 	,dataDeposito DATE
 	,horaDeposito SMALLDATETIME
 	,idNumConta INT FOREIGN KEY REFERENCES tbContaCorrente(idNumConta)
@@ -25,7 +25,7 @@ CREATE TABLE tbDeposito(
 
 CREATE TABLE tbSaque(
 	idSaque INT PRIMARY KEY IDENTITY(1, 1)
-	,valorSaque VARCHAR(15)
+	,valorSaque MONEY
 	,dataSaque DATE
 	,horaSaque SMALLDATETIME
 	,idNumConta INT FOREIGN KEY REFERENCES tbContaCorrente(idNumConta)
