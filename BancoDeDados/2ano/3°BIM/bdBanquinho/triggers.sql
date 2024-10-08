e9fe09186ae4c06227f2e6e56aa115a55f22d96e
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-08 15:50:29 -0300

    Create triggers.sql

---
+++ BancoDeDados/2ano/3°BIM/bdBanquinho/triggers.sql
@@ -0,0 +1,9 @@
+/*2 - Criar dois triggers que:
+A) Ao ser feito um depósito atualize o saldo da conta corrente, somando à quantia depositada*/
+
+
+
+
+
+
+--B) Ao ser feito um saque atualize o saldo da conta corrente, descontando o valor da última (caso haja saldo bastante)
