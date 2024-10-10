b9e611d441aef2b80bafe92116867805b970f049
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-10 11:56:32 -0300

    Update triggers.sql

--- BancoDeDados/2ano/3°BIM/bdBanquinho/triggers.sql
+++ BancoDeDados/2ano/3°BIM/bdBanquinho/triggers.sql
@@ -24,33 +24,49 @@ AS
 SELECT * FROM tbContaCorrente
 SELECT * FROM tbDeposito
 
+	-- Inserts p/ teste dessa e da próxima questões
 INSERT INTO tbDeposito (valorDeposito, dataDeposito, horaDeposito, idNumConta)
-	VALUES(1125, '14-12-2024', '14-12-2024 10:06:00', 1)
+	VALUES(1125, '14-12-2024', '14-12-2024 10:06:00', 2)
+	
+INSERT INTO tbDeposito (valorDeposito, dataDeposito, horaDeposito, idNumConta)
+	VALUES(200, '09-12-2024', '09-12-2024 22:33:00', 1)
 
 /*	DROP TRIGGER tgAtualizaSaldoConCor	*/
 	
+/*	DROP TRIGGER tgMinusSaldoContaCorrente 	*/
+	
 --B) Ao ser feito um saque atualize o saldo da conta corrente, descontando o valor da última (caso haja saldo bastante)
-CREATE TRIGGER tgAtualizaSaldoContaCorrente 
+CREATE TRIGGER tgMinusSaldoContaCorrente 
 ON tbSaque
 AFTER INSERT
 AS
 
-	 DECLARE @idNumConta INT, @idSaque INT, @valSaque INT
+	 DECLARE @idNumConta INT, @idSaque INT, @valSaque MONEY, @saldCont MONEY
  
-	 SET @idNumConta = (SELECT idNumConta FROM tbSaque WHERE idNumConta = @idNumConta)
+	 SET @idNumConta = (SELECT idNumConta FROM INSERTED)
 
-	 SELECT @idSaque = idSaque
+	 SET @saldCont = (SELECT saldoConta FROM tbContaCorrente WHERE idNumConta = @idNumConta)
+
+	 SELECT @idSaque = idSaque, @valSaque = valorSaque FROM INSERTED
  
-	 SELECT @valSaque = valorSaque
- 
+	IF (@valSaque > @saldCont)
+	BEGIN
+		PRINT('Não foi possível efetuar o saque pois o valor de R$' + @valSaque +' é superior ao saldo atual da conta - a saber: R$' + @saldCont + '.')
+	END
+	ELSE
+	BEGIN
 	 UPDATE tbContaCorrente
 		SET saldoConta = saldoConta - @valSaque
 		 WHERE idNumConta = @idNumConta
-
+	END
 
 	-- Testando Trigger
 SELECT * FROM tbContaCorrente
 SELECT * FROM tbSaque
+SELECT * FROM tbDeposito
 
 INSERT INTO tbSaque (valorSaque, dataSaque, horaSaque, idNumConta)
 	VALUES(55, '15-12-2024', '15-12-2024 17:49:00', 1)
+	
+INSERT INTO tbSaque (valorSaque, dataSaque, horaSaque, idNumConta)
+	VALUES(150, '17-12-2024', '17-12-2024 15:53:00', 1)
