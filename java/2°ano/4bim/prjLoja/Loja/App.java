87be14b12a3322497685877331b76b66eb9abd37
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-12 13:09:31 -0300

    Create App.java

---
+++ java/2°ano/4bim/prjLoja/Loja/App.java
@@ -0,0 +1,20 @@
+
+public class App {
+
+	public static void main(String[] args) {	
+			new Menu();
+			
+			/*Menu
+BdLoja
+    Arquivo 
+       Sair
+    Cadastrar
+       Produto
+       Cliente
+       Pedido
+    Ajuda
+       Sobre*/
+		
+	}
+
+}
