a77d3e79ae0028963b3f944caa94e05c60675db9
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-13 16:14:57 -0300

    Update banco.sql

--- PW2/LoginAula/banco.sql
+++ PW2/LoginAula/banco.sql
@@ -3,10 +3,12 @@ CREATE DATABASE bdLoginAula;
 --DROP DATABASE bdLoginAula;
 
 USE bdLoginAula;
-/* Tabela das contas geradas no site*/
+
+--DROP TABLE tbUsuario;
+/* Tabela das contas geradas no site */
 CREATE TABLE tbUsuario (
     idUsuario INT PRIMARY KEY AUTO_INCREMENT
     ,nomeUsuario VARCHAR (60)
     ,senhaUsuario VARCHAR (255) /* Ideal para armazenar Hash*/
-    ,tipoUsuario INT
+    ,tipoUsuario VARCHAR(4)
 );
