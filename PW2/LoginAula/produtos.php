393ba8ffe09f42c12951b5741fa825137569518b
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-13 18:17:16 -0300

    Create produtos.php

---
+++ PW2/LoginAula/produtos.php
@@ -0,0 +1,15 @@
+<?php 
+
+include ('verificarLogin.php');
+
+echo "<body style='background-color:LightBlue'>
+       <h1>Produtos</h1>
+        <a href=clientes.php>Clientes</a>
+        <a href=logout.php>Sair</a>";
+
+echo "        <div class=devs>
+        <h1>DESENVOLVEDORES</h1>
+        <a href=desenvolvedores.php>DESENVOLVEDORES</a>
+        </div>
+
+    </body>";
