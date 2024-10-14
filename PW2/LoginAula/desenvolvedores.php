d2f9aad4c1560fe6f87d92645292798c53d83d50
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-13 23:44:33 -0300

    Update desenvolvedores.php

--- PW2/LoginAula/desenvolvedores.php
+++ PW2/LoginAula/desenvolvedores.php
@@ -1,22 +1,19 @@
 <?php 
+include ('conexao.php'); //conecta ao banco
+include ('verificarLogin.php'); //verifica se está logado
 
-include ('verificarLogin.php');
-
-if (!isset($_SESSION['tipo'])) {
-    header('Location: cliente.php?erro=true');
-    exit;
-}
-
-if ($_SESSION['tipo'] == 'user') { //Usuário
+//Usuário 
+if ($_SESSION['tipo'] == 'user') { //Limita o acesso do usuário normal
     header('Location: clientes.php?negado=true');
-    exit;
-} else if ($_SESSION['tipo'] == 'dev') { //Desenvolvedor
-    echo "<body style='background-color:LightBlue'>
+    exit; 
+
+//Desenvolvedor
+} else if ($_SESSION['tipo'] == 'dev') { ?>
+        <body>
           <h1>Bem Vindo</h1>
           <h1>Página Somente Para Devs</h1>
           <a href='clientes.php'>Clientes</a>
           <a href='produtos.php'>Produtos</a>
           <a href='logout.php'>Sair</a>
-          </body>";
-}
-?>
+        </body>
+<?php } ?>
