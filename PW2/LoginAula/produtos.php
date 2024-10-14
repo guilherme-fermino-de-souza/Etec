64eb94f5101cabe33cbea6d772752396444c3a5b
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-13 23:46:06 -0300

    Update produtos.php

--- PW2/LoginAula/produtos.php
+++ PW2/LoginAula/produtos.php
@@ -1,15 +1,30 @@
 <?php 
+include ('conexao.php'); //conecta ao banco
+include ('verificarLogin.php'); //verifica se está logado
 
-include ('verificarLogin.php');
+//Usuário 
+if ($_SESSION['tipo'] == 'user') { //Limita o acesso do usuário normal
+    header('Location: clientes.php?negado=true');
+    exit; 
 
-echo "<body style='background-color:LightBlue'>
+//Desenvolvedor
+} else if ($_SESSION['tipo'] == 'dev') { ?>
        <h1>Produtos</h1>
         <a href=clientes.php>Clientes</a>
-        <a href=logout.php>Sair</a>";
+        <a href=logout.php>Sair</a>
 
-echo "        <div class=devs>
+       <div class=devs>
         <h1>DESENVOLVEDORES</h1>
         <a href=desenvolvedores.php>DESENVOLVEDORES</a>
-        </div>
+       </div>
 
-    </body>";
+       <?php           
+        $stmt = $pdo->prepare("SELECT * FROM tbUsuario WHERE idUsuario = ?");
+        $idUsuario = $_SESSION['id'];
+        $stmt -> execute([$idUsuario]);
+        while($row = $stmt->fetch(PDO::FETCH_BOTH)){?>
+        
+        <h1>Nome: <?php echo $row["nomeUsuario"]; ?></h1>
+        <h2>Tipo Conta: <?php echo $row["tipoUsuario"]; ?></h2>
+
+<?php }} ?>
