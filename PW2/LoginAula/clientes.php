3b5dddf675ddde3bd894eaccbf5f19d1625af71d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-13 23:42:41 -0300

    Update clientes.php

--- PW2/LoginAula/clientes.php
+++ PW2/LoginAula/clientes.php
@@ -1,20 +1,31 @@
 <?php 
 
+include ('verificarLogin.php'); //verifica o login
+
 $negado = ''; // Inicializa a mensagem de erro para caso acesso negado
 
-if (isset($_GET['negado'])) {
+if (isset($_GET['negado'])) { // Quando o user tenta entrar em páginas restritas
     $negado = "Você não possui acesso a essa página!";
 }
+?>
 
-include ('verificarLogin.php');
+<h1>Clientes</h1>
 
-echo "<body style='background-color:LightYellow'>
-       <h1>Clientes</h1>
-        <a href=produtos.php>Produtos</a>
-        <a href=logout.php>Sair</a>
-    </body>";
-    ?>
+<!-- Sempre visível -->
+<a href="desenvolvedores.php">Desenvolvedor</a>
 
+
+<?php if ($_SESSION['tipo'] === 'dev'): ?> <!-- Visível somente para os devs-->
+    <a href="produtos.php">Produtos</a>
+
+<?php else: ?><!-- Bloqueia produtos.php caso seja um user -->   
+    <span style="color:gray;">Produtos (Acesso Negado)</span>
+
+<?php endif; ?>
+
+<a href="logout.php">Sair</a><!-- Sai da SESSION-->
+
+<!-- Aviso ativado quando o $negado receber um valor true -->
 <div style="background-color:coral; margin:10px">
     <?php echo $negado ?? ''; ?>
 </div>
