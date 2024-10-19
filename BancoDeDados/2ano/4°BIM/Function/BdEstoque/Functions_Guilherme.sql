90c2eaefc08623ab10bdc2f978d11bafc0e2a2a2
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-19 15:48:59 -0300

    Update and rename Functions_Guilherme to Functions_Guilherme.sql

---
+++ BancoDeDados/2ano/4°BIM/Function/BdEstoque/Functions_Guilherme.sql
@@ -0,0 +1,107 @@
+-- 1 Criar uma função que retorne o dia de semana da venda (no formato segunda, terça, etc) ao lado do código da venda, valor total da venda e sua data
+CREATE FUNCTION fc_DiaVenda(@diaVenda DATE) RETURNS VARCHAR(40) AS 
+	BEGIN
+	DECLARE @Dia INT
+	SET @Dia = DATEPART(dw, @DiaVenda)
+
+	IF @Dia = 1 BEGIN
+		SET @diaVenda = 'Domigo'
+	END
+	IF @Dia = 2 BEGIN
+		SET @diaVenda = 'Segunda'
+	END
+	IF @Dia = 3 BEGIN
+		SET @diaVenda = 'Terça'
+	END
+	IF @Dia = 4 BEGIN
+		SET @diaVenda = 'Quarta'
+	END
+	IF @Dia = 5 BEGIN
+		SET @diaVenda = 'Quinta'
+	END
+	IF @Dia = 6 BEGIN
+		SET @diaVenda = 'Sexta'
+	END
+		IF @Dia = 7 BEGIN
+		SET @diaVenda = 'Sábado'
+	END
+
+	RETURN @diaVenda
+END
+
+-- DROP FUNCTION fc_DiaVenda --
+-- SELECT dataVenda AS 'Data', idVenda, valorTotalVenda, Dia=dbo.fc_DiaVenda(dataVenda) FROM tbVenda --
+
+-- 2 Criar uma função que receba o código do cliente e retorne o total de vendas que o cliente já realizou --
+
+
+
+-- 3 Criar uma função que receba o código de um vendedor e o mês e informe o total de vendas do vendedor no mês informado --
+
+
+
+-- 4 Criar uma função que usando o bdEstoque diga se o cpf do cliente é ou não válido
+CREATE FUNCTION fc_VerificarCpf(@cpf VARCHAR(11)) RETURNS CHAR(1) AS
+	BEGIN
+		DECLARE @indice INT
+			,@soma INT
+			,@dig1 INT
+			,@dig2 INT
+			,@cpfTemporario INT
+			,@digitosIguais INT
+			,@resultados CHAR(1)
+
+		SET @resultados = 'N'
+	-- Não pode o mermo número repetido --
+		SET @cpfTemporario = SUBSTRING(@cpf, 1,1) -- Extrai o primeiro caractere do @cpf --
+
+		SET @indice = 1 -- valor 1 para começar no primeiro caractere --
+		SET @digitosIguais = 'S' -- @digitosIguais começa como verdadeiro --
+
+		WHILE (@indice <= 11)
+			BEGIN
+				IF SUBSTRING(@cpf, @indice, 1) <> @cpfTemporario -- '<>' = diferente --
+					SET @digitosIguais = 'N'
+					SET @indice = @indice + 1 -- Aumenta o @indice para pegar todos os caracteres do @cpf --
+			END
+		-- Caso números diferentes --
+		IF @digitosIguais = 'N'
+			BEGIN
+				-- Calculo 1° Dígito
+				SET @soma = 0
+				SET @indice = 1 -- Retorna para 1 --
+				WHILE (@indice <= 9)
+					BEGIN
+						SET @soma = @soma + CONVERT(INT, SUBSTRING(@cpf, @indice, 1)) * (11 - @indice)
+						SET @indice = @indice + 1 -- aumenta o @indice --
+					END
+
+				SET @dig1 = 11 - (@soma % 11)
+
+				IF @dig1>9
+					SET @dig1=0
+
+				-- Calculo 2° Dígito
+				SET @soma = 0
+				SET @indice = 1 -- Retorna para 1(dnv) --
+				WHILE (@indice <= 10)
+					BEGIN
+						SET @soma = @soma + CONVERT(INT, SUBSTRING(@cpf, @indice, 1)) * (12 - @indice)
+						SET @indice = @indice + 1
+					END
+				SET @dig2 = 11 - (@soma % 11)
+
+				IF @dig2 > 9
+					SET @dig2=0
+
+				-- Validando --
+				IF (@dig1 = SUBSTRING(@cpf, LEN(@cpf)-1,1)) AND (@dig2=SUBSTRING(@cpf, LEN(@cpf),1))
+					SET @resultados = 'S'
+				ELSE
+					SET @resultados = 'N'
+				END
+RETURN @resultados
+END
+
+-- DROP FUNCTION fc_VerificarCpf --
+-- SELECT cpfCliente AS 'CPF', válido=dbo.fc_VerificarCpf(cpfCliente) FROM tbCliente --
