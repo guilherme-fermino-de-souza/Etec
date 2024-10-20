ed24df01ce8ad71ce6e64bc064bfcc5a5cb135fa
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-20 14:52:53 -0300

    Create criarNoticia.php

---
+++ PW2/NoticiasAula/criarNoticia.php
@@ -0,0 +1,7 @@
+<form action="inserirNoticia.php" method="post">
+    <input type="text" name="titulo" placeholder="título">
+    <input type="text" name="subtitulo" placeholder="subtítulo">
+    <input type="text" name="descricao" placeholder="descrição">
+    <input type="file" name="img" accept="phg" placeholder="img.png">
+    <input type="submit" valu="Enviar">
+</form>
