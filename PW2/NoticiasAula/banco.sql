a7d397cc31ff370e4f65780b7e294d0d36f34f7c
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-20 12:43:54 -0300

    Create banco.sql

---
+++ PW2/NoticiasAula/banco.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE dbNoticias
+
+CREATE TABLE tbNoticias (
+    idNoticias INT PRIMARY KEY AUTO_INCREMENT
+    ,tituloNoticias VARCHAR(100)
+    ,subtituloNoticias VARCHAR(100)
+    .descNoticias VARCHAR(555)
+    ,imgNoticias INT
+);
+
+CREATE TABLE tbParagrafo (
+    idParagrafo INT PRIMARY KEY AUTO_INCREMENT
+    ,textoParagrafo VARCHAR(1555)
+    ,noticias_id
+    ,FOREIGN KEY (noticias_id) REFERENCES tbNoticias(idNoticias)
+);
