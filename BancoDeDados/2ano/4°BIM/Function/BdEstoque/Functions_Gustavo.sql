062ad5b565a3efc427b0d0efcfd5f4e2cc227f7f
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-20 11:02:48 -0300

    Create Functions_Gustavo.sql

---
+++ BancoDeDados/2ano/4°BIM/Function/BdEstoque/Functions_Gustavo.sql
@@ -0,0 +1,48 @@
+/* 5. Criar uma função que retorne o dia de semana da venda (no formato segunda,
+terça, etc) ao lado do código da venda, valor total da venda e sua data*/
+CREATE FUNCTION fc_diaSemanaVenda(@dataVenda DATETIME)
+	RETURNS VARCHAR(40)
+AS
+BEGIN
+	DECLARE @diaSemana AS VARCHAR(40), @dia INT	/*, @idVenda INT, @valTotalVenda MONEY	Só permite 1 retorno ._.
+	SET @dataVenda = (SELECT dataVenda FROM tbVenda WHERE idvenda = @idVenda)*/
+	SET @dia = DATEPART(DW, @dataVenda)
+	IF @dia = 1
+		BEGIN
+		SET @diaSemana = 'Domingo.'
+		END
+	IF @dia = 2
+		BEGIN
+		SET @diaSemana = 'Segunda-feira.'
+		END
+	IF @dia = 3
+		BEGIN
+		SET @diaSemana = 'Terça-feira.'
+		END
+	IF @dia = 4
+		BEGIN
+		SET @diaSemana = 'Quarta-feira.'
+		END
+	IF @dia = 5
+		BEGIN
+		SET @diaSemana = 'Quinta-feira.'
+		END
+	IF @dia = 6
+		BEGIN
+		SET @diaSemana = 'Sexta-feira.'
+		END
+	IF @dia = 7
+		BEGIN
+		SET @diaSemana = 'Sábado.'
+		END
+
+	/*		Estava achando que era Stored Procedure ainda.
+	SET @valTotalVenda = (SELECT valorTotalVenda FROM tbVenda WHERE idvenda = @idVenda)
+	DECLARE @converDataVen VARCHAR(12)
+	SET @converDataVen = CONVERT(VARCHAR(10), @dataVenda, 103)	// + @idVenda + @valTotalVenda + @converDataVen
+	DROP FUNCTION dbo.fc_diaSemanaVenda*/
+
+	RETURN @diaSemana 
+END
+
+SELECT diaDaSemana = dbo.fc_diaSemanaVenda(dataVenda), idvenda, valorTotalVenda, dataVenda FROM tbVenda
