4ab7c92a46c39fd0f9224c166503d7a513003478
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-27 01:18:29 -0300

    Update Aplicacao.java

--- java/2°ano/4bim/ExemploDao/Aplicacao.java
+++ java/2°ano/4bim/ExemploDao/Aplicacao.java
@@ -1,9 +1,23 @@
 package prjExemploDao;
+/*Data Acess Object
+(Objeto de Acesso a Dados) */
+
+import java.sql.SQLException;
+import Model.Produto;
+import DAO.ProdutoDao;
 
 public class Aplicacao {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException{
+
+		Produto produto = new Produto(); //1° Objeto
+		
+		produto.setProduto("Biscoito");
+		produto.setValorProduto(4);
+		
+		ProdutoDao produtoDao = new ProdutoDao(); //2° Objeto
+		produtoDao.adicionar(produto);
+		
 
 	}
 
