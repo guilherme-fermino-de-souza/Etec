f55bd4e0ea11eb7db4e2832026388a99a394508d
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-27 21:23:45 -0300

    Update ProdutoDao.java

--- java/2°ano/4bim/ExemploDao/ProdutoDao.java
+++ java/2°ano/4bim/ExemploDao/ProdutoDao.java
@@ -2,8 +2,11 @@ package DAO; /*Data Acess Object
 (Objeto de Acesso a Dados) do Produto */
 
 import java.sql.SQLException;
+import java.util.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import Model.Produto; //Importação do Produto
 
@@ -13,9 +16,10 @@ public class ProdutoDao {
 	
 	public ProdutoDao() { //Construtor(O mesmo nome da classe)
 		this.connection = new ConectionFactory().getConnection();
-		//Gera conexão com o banco
+		//Gera conexão com o banco toda vez que se instacia um objeto
 	}
 	
+	//Adicionar Produto Banco De Dados
 	public void adicionar(Produto produto) throws SQLException {
 		try {
 			String sql = "insert into tbProduto"+
@@ -36,5 +40,34 @@ public class ProdutoDao {
 			connection.close();
 		}
 	}
+	
+	//Lista Banco De Dados
+	public List<Produto> getLista() throws SQLException{
+		try {
+			List<Produto> produtos = new ArrayList<Produto>(); //Define a Lista
+			
+			PreparedStatement stmt = this.connection.prepareStatement("select * from tbProduto"); //Prepara a instrução MySQL
+			ResultSet rs = stmt.executeQuery(); //Armazena e Executa instrução em RS
+			
+			while(rs.next()) { // Busca todos o objetos armazenados no BD
+				Produto produto = new Produto();
+				produto.setIdProduto(rs.getInt(1)); //Id
+				produto.setProduto(rs.getString(2)); //Nome
+				produto.setValorProduto(rs.getDouble(3)); //Valor
 
+				produtos.add(produto); //Cria um objeto para cada produto buscado
+			}
+			
+			rs.close();
+			stmt.close();
+			
+			return produtos;
+		} 
+		catch(SQLException e) {
+			throw new RuntimeException();
+		}
+		finally {
+			connection.close();
+		}
+	}
 }
