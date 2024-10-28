75aaeeca83d8b6ccfd1b64901bf3c16709238f4c
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-28 15:47:33 -0300

    Update Loja.sql
    28/10/24

--- java/2°ano/4bim/prjLoja/Loja/Banco/Loja.sql
+++ java/2°ano/4bim/prjLoja/Loja/Banco/Loja.sql
@@ -1,5 +1,7 @@
 -- Ligar Eclipse ao MySql --
 CREATE DATABASE bdLoja
+
+    -- DROP DATABASE bdLoja --
 -- Cadastrar Cliente --
 CREATE TABLE tbCliente( -- Tabela Cliente
     idCliente INT PRIMARY KEY AUTO_INCREMENT
@@ -26,6 +28,7 @@ CREATE TABLE tbProduto( -- Tabela Produto
     idProduto INT PRIMARY KEY AUTO_INCREMENT
     ,nomeProduto VARCHAR(30)
     ,valorProduto DOUBLE
+    ,quantidadeProduto INT
     ,categoriaProduto_id INT
     ,FOREIGN KEY (categoriaProduto_id) REFERENCES tbCategoriaProduto(idCategoriaProduto)
 );
@@ -34,3 +37,19 @@ CREATE TABLE tbCategoriaProduto( -- Tabela Categoria Produto
     idCategoriaProduto INT PRIMARY KEY AUTO_INCREMENT
     nomeCategoriaProduto VARCHAR(35)
 );
+
+-- Cadastrar Pedido --
+CREATE TABLE tbPedido( -- Tabela Pedido
+	idPedido INT PRIMARY KEY AUTO_INCREMENT
+   ,dataPedido DATE 
+   ,valorTotalPedido DOUBLE
+   ,dataEntregaEncomenda SMALLDATETIME 
+   ,idCliente INT FOREIGN KEY REFERENCES tbCliente(idCliente)
+)
+
+CREATE TABLE tbItensPedido( -- Tabela Itens Pedido 
+	idItensPedido INT PRIMARY KEY AUTO_INCREMENT
+	,quantidadeProdutoItensPedido INT
+	,idPedido INT FOREIGN KEY REFERENCES tbPedido(idPedido)
+	,idProduto INT FOREIGN KEY REFERENCES tbProduto(idProduto)
+)
