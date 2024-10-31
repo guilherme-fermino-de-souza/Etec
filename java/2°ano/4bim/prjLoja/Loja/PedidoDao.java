447fb8727a1314f1ce2e07230844781e5f4f4c7b
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-10-31 02:12:34 -0300

    Update PedidoDao.java

--- java/2°ano/4bim/prjLoja/Loja/PedidoDao.java
+++ java/2°ano/4bim/prjLoja/Loja/PedidoDao.java
@@ -1,5 +1,5 @@
 package DAO; /*Data Acess Object
-(Objeto de Acesso a Dados) do Produto */
+(Objeto de Acesso a Dados) do Pedido */
 
 import java.sql.SQLException;
 import java.util.List;
@@ -10,84 +10,126 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.time.LocalDate; //Permite variaveis tipo Data
+import java.time.format.DateTimeFormatter; //Converte o formato do dado
 import Model.Pedido; //Importação do Pedido
 
 public class PedidoDao {
 	
 	private Connection connection;
 	
-	public ProdutoDao() { //Construtor(O mesmo nome da classe)
+	public PedidoDao() { //Construtor(O mesmo nome da classe)
 		this.connection = new ConectionFactory().getConnection();
 		//Gera conexão com o banco toda vez que se instacia um objeto
 	}
 	
-	//ADICIONAR Categoria Produto Banco De Dados
-	public void adicionarCategoriaProduto(Produto categoriaProduto) throws SQLException {
+	//ADICIONAR Pedido Banco De Dados
+	public void adicionarPedido(Pedido pedido) throws SQLException {
 		try {
-			String sql = "insert into tbCategoriaProduto"+
-				"(nomeCategoriaProduto)"+
-				"values (?)";
-			
-			PreparedStatement stmt = connection.prepareStatement(sql);
-			stmt.setString(1, categoriaProduto.getNomeCategoriaProduto());
-			stmt.execute();
-			stmt.close();
-			JOptionPane.showMessageDialog(null,"Categoria Produto Cadastrado Com Sucesso");
-		} 
-		catch(SQLException e) {
-			System.out.println("Erro ao registrar Categoria Produto: "+e);
-		}
-		finally {
-			connection.close();
-		}
-	}
-	
-	//ADICIONAR Produto Banco De Dados
-	public void adicionarProduto(Produto produto) throws SQLException {
-		try {
-			String sql = "insert into tbProduto"+
-				"(nomeProduto, valorProduto, quantidadeProduto, categoriaProduto_id)"+
+			String sql = "insert into tbPedido"+
+				"(dataPedido, valorTotalPedido, dataEntregaPedido, cliente_id)"+
 				"values (?, ?, ?, ?)";
 			
 			PreparedStatement stmt = connection.prepareStatement(sql);
-			stmt.setString(1, produto.getNomeProduto());
-			stmt.setDouble(2, produto.getValorProduto());
-			stmt.setInt(3, produto.getQuantidadeProduto());
-			stmt.setInt(4, produto.getIdCategoriaProduto());
+			
+			stmt.setDate(1, java.sql.Date.valueOf(pedido.getDataPedido()));
+			stmt.setDouble(2, pedido.getValorPedido());
+			stmt.setDate(3, java.sql.Date.valueOf(pedido.getDataEntregaPedido()));
+			stmt.setInt(4, pedido.getIdCliente());
 			stmt.execute();
 			stmt.close();
-			JOptionPane.showMessageDialog(null,"Produto Cadastrado Com Sucesso");
+			JOptionPane.showMessageDialog(null,"Pedido Cadastrado Com Sucesso");
 		} 
 		catch(SQLException e) {
-			System.out.println("Erro ao registrar Produto: "+e);
+			System.out.println("Erro ao registrar Pedido: "+e);
 		}
 		finally {
 			connection.close();
 		}
 	}
 	
-	//CONSULTA Banco De Dados
-	public List<Produto> getLista() throws SQLException{
+	//ADICIONAR Itens Pedido Banco De Dados
+	public void adicionarItensPedido(Pedido itensPedido) throws SQLException {
 		try {
-			List<Produto> produtos = new ArrayList<Produto>(); //Define a Lista
+			String sql = "insert into tbItensPedido"+
+				"(quantidadeItensPedido, pedido_id, produto_id)"+
+				"values (?, ?, ?)";
 			
-			PreparedStatement stmt = this.connection.prepareStatement("select * from tbProduto"); //Prepara a instrução MySQL
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setInt(1, itensPedido.getQuantidadePedido());
+			stmt.setInt(2, itensPedido.getIdPedido());
+			stmt.setInt(3, itensPedido.getIdProduto());
+			stmt.execute();
+			stmt.close();
+			JOptionPane.showMessageDialog(null,"Itens Pedido Cadastrado Com Sucesso");
+		} 
+		catch(SQLException e) {
+			System.out.println("Erro ao registrar Itens Pedido: "+e);
+		}
+		finally {
+			connection.close();
+		}
+	}
+	
+	//CONSULTA tbPedido
+	public List<Pedido> getListaPedido() throws SQLException{
+		try {
+			List<Pedido> pedidos = new ArrayList<Pedido>(); //Define a Lista
+			
+			PreparedStatement stmt = this.connection.prepareStatement("select * from tbPedido"); //Prepara a instrução MySQL
 			ResultSet rs = stmt.executeQuery(); //Armazena e Executa instrução em RS
 			
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			
 			while(rs.next()) { // Busca todos o objetos armazenados no BD
-				Produto produto = new Produto();
-				produto.setIdProduto(rs.getInt(1)); //Id
-				produto.setProduto(rs.getString(2)); //Nome
-				produto.setValorProduto(rs.getDouble(3)); //Valor
+				Pedido pedido = new Pedido();
+				pedido.setIdPedido(rs.getInt(1)); //Id
+				String txDataPedido = rs.getString(2); //Data
+				LocalDate dataPedido = LocalDate.parse(txDataPedido, formatter);
+				pedido.setDataPedido(dataPedido);
+				pedido.setValorPedido(rs.getDouble(3)); //Valor
+				String txDataEntregaPedido = rs.getString(2); //Data Entrega
+				LocalDate dataEntregaPedido = LocalDate.parse(txDataEntregaPedido, formatter);
+				pedido.setDataEntregaPedido(dataEntregaPedido);
+				pedido.setIdCliente(rs.getInt(5)); //Id Cliente
 
-				produtos.add(produto); //Cria um objeto para cada produto buscado
+				pedidos.add(pedido); //Cria um objeto para cada produto buscado
 			}
 			
 			rs.close();
 			stmt.close();
 			
-			return produtos;
+			return pedidos;
+		} 
+		catch(SQLException e) {
+			throw new RuntimeException("Erro ao consultar tabela", e);
+		}
+		finally {
+			connection.close();
+		}
+	}
+	
+	//CONSULTA tbItensPedido
+	public List<Pedido> getListaItensPedido() throws SQLException{
+		try {
+			List<Pedido> itensPedidos = new ArrayList<Pedido>(); //Define a Lista
+			
+			PreparedStatement stmt = this.connection.prepareStatement("select * from tbItensPedido"); //Prepara a instrução MySQL
+			ResultSet rs = stmt.executeQuery(); //Armazena e Executa instrução em RS
+			
+			while(rs.next()) { // Busca todos o objetos armazenados no BD
+				Pedido pedido = new Pedido();
+				pedido.setQuantidadePedido(rs.getInt(1)); //Quantidade
+				pedido.setIdPedido(rs.getInt(2)); //Id Pedido
+				pedido.setIdProduto(rs.getInt(3)); //Id Produto
+
+				itensPedidos.add(pedido); //Cria um objeto para cada produto buscado
+			}
+			
+			rs.close();
+			stmt.close();
+			
+			return itensPedidos;
 		} 
 		catch(SQLException e) {
 			throw new RuntimeException("Erro ao consultar tabela", e);
