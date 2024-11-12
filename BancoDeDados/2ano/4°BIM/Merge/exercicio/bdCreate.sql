8a550724b0dcc916c44906a065477d140962e4bc
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-11-12 18:28:41 -0300

    Create bdCreate.sql

---
+++ BancoDeDados/2ano/4°BIM/Merge/exercicio/bdCreate.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE bdEtecQualqMerge
+
+-- DROP DATABASE bdEtecQualqMerge
+  CREATE TABLE tbAlunos2a(
+  rmAluno INT PRIMARY KEY
+  , nomeAluno VARCHAR(36)
+  , statusAluno VARCHAR(10)
+)
+
+CREATE TABLE tbAlunos2b(
+  rmAluno INT PRIMARY KEY
+  , nomeAluno VARCHAR(36)
+  , statusAluno VARCHAR(10)
+)
+
+
+CREATE TABLE tbAlunos3ano(
+	rmAluno INT PRIMARY KEY
+  , nomeAluno VARCHAR(36)
+  , statusAluno VARCHAR(10)
+)
