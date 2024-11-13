6611fc542da51ed7c0845dbbcbb0710447988b30
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-11-13 18:39:32 -0300

    Update bdMerge.sql

--- BancoDeDados/2ano/4°BIM/Merge/exercicio/bdMerge.sql
+++ BancoDeDados/2ano/4°BIM/Merge/exercicio/bdMerge.sql
@@ -1,4 +1,15 @@
 -- TESTE, dps FINAL
 
-
+--MERGE tb
+MERGE tbAlunos3ano desti
+	USING  tbAlunos2a ori
+	ON ori.statusAluno = 'APROVADO'
+	WHEN NOT MATCHED THEN	
+		IF (SELECT rmAluno FROM tbAluno2a WHERE statusAluno = 'APROVADO')
+		BEGIN
+			INSERT VALUES (ori.rmAluno, ori.nomeAluno, ori.statusAluno)
+		ELSE
+		 UPDATE ori.statusAluno = 'RETIDO'
+--	WHEN NOT MATCHED THEN
+--		UPDATE SET ori.statusAluno = 'RETIDO';
 
