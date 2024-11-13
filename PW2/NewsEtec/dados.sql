0968b5b6204e7e3860446389600eaf455c9e170b
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-11-13 19:03:08 -0300

    Update and rename dados to dados.sql

---
+++ PW2/NewsEtec/dados.sql
@@ -0,0 +1,91 @@
+CREATE DATABASE bdNewsWallEtec;
+
+--DROP DATABASE bdNewsWallEtec;
+
+USE bdNewsWallEtec;
+/* Tabela das notícias inseridas pelos admins*/
+CREATE TABLE tbNoticias (
+    idNoticias INT PRIMARY KEY AUTO_INCREMENT
+    ,tituloNoticias VARCHAR (150)
+    ,subtituloNoticias VARCHAR (200)
+    ,descNoticias VARCHAR (500)
+    ,imgNoticias INT
+);
+/* Tabela das contas registradas em 'nova conta'*/
+CREATE TABLE tbConta (
+    idConta INT PRIMARY KEY AUTO_INCREMENT
+    ,nomeConta VARCHAR (150)
+    ,emailConta VARCHAR (60)
+    ,senhaConta VARCHAR (255)
+    ,tipoConta VARCHAR (4)
+);
+/* Tabela das mensagens recebidas pelo contato*/
+CREATE TABLE tbContato (
+    idContato INT PRIMARY KEY AUTO_INCREMENT
+    ,nomeContato VARCHAR (90)
+    ,emailContato VARCHAR (30)
+    ,assuntoContato VARCHAR (40)
+    ,mensagemContato VARCHAR (250)
+);
+
+/* Tabela dos comentários da notícia */
+CREATE TABLE tbComentarioNoticia (
+    idComentarioNoticia INT PRIMARY KEY AUTO_INCREMENT,
+    nomeComentarioNoticia VARCHAR(40),
+    mensagemComentarioNoticia VARCHAR(400),
+    noticia_id INT,
+    FOREIGN KEY (Noticia_id) REFERENCES tbNoticias(idNoticias)
+);
+
+/* PARTE NOTÍCIAS */
+
+/* Paragrafo ligado a tbNoticias */
+CREATE TABLE tbParagrafoNoticias (
+    idParagrafoNoticias INT PRIMARY KEY AUTO_INCREMENT
+    ,textoParagrafoNoticias VARCHAR(1750)
+    ,noticia_id INT
+    ,FOREIGN KEY (Noticia_id) REFERENCES tbNoticias(idNoticias)
+);
+
+/* imagens ligado a tbNoticias*/
+CREATE TABLE tbImagenNoticia (
+    idImagenNoticia INT PRIMARY KEY AUTO_INCREMENT
+    ,imgImagenNoticia INT
+    ,noticia_id INT
+    ,FOREIGN KEY (Noticia_id) REFERENCES tbNoticias(idNoticias)
+);
+
+
+/*Projeto NewsEtec
+
+  Páginas
+   index, login, usuario, administrador e professor.
+     devem contar com navbar, menu de opções (admin, user, login e pág inicial) e footer
+
+       aluno(usuário)
+        codUser
+        nomeUser
+        rgUser
+        cpfUser
+        dataNasUser
+        lograUser...
+        turmaAluno
+
+       professor(prof)
+        codProf
+        nomeProf
+        cpfProf
+        rgProf
+        dataNasProf
+        lograProf
+        materiaProf
+        periodosProf
+        salasQueDaAula
+
+       direcao(admin)
+        codAdmin
+        nomeAdmin
+        cpfAdmin
+        rgAdmin
+        dataNasAdmin
+        lograAdmin
