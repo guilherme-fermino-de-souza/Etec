2e5de82712ac7d2a80623d58fca164cef4c954ed
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-11-13 20:58:36 -0300

    Update bdMerge.sql

--- BancoDeDados/2ano/4°BIM/Merge/exercicio/bdMerge.sql
+++ BancoDeDados/2ano/4°BIM/Merge/exercicio/bdMerge.sql
@@ -1,15 +1,9 @@
--- TESTE, dps FINAL
-
---MERGE tb
 MERGE tbAlunos3ano desti
-	USING  tbAlunos2a ori
-	ON ori.statusAluno = 'APROVADO'
-	WHEN NOT MATCHED THEN	
-		IF (SELECT rmAluno FROM tbAluno2a WHERE statusAluno = 'APROVADO')
-		BEGIN
-			INSERT VALUES (ori.rmAluno, ori.nomeAluno, ori.statusAluno)
-		ELSE
-		 UPDATE ori.statusAluno = 'RETIDO'
---	WHEN NOT MATCHED THEN
---		UPDATE SET ori.statusAluno = 'RETIDO';
-
+	USING (
+		SELECT rmAluno, nomeAluno, statusAluno FROM tbAlunos2a WHERE statusAluno = 'APROVADO'
+			UNION ALL
+		SELECT rmAluno, nomeAluno, statusAluno FROM tbAlunos2b WHERE statusAluno = 'APROVADO'
+	) ori
+	ON desti.rmAluno = ori.rmAluno
+	WHEN NOT MATCHED BY TARGET THEN
+		INSERT (rmAluno, nomeAluno, statusAluno) VALUES (ori.rmAluno, ori.nomeAluno, ori.statusAluno);
