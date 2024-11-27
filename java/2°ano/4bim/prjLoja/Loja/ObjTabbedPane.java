f19e7d570e17f344ee2e1f9488ffdaeba1822950
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2024-11-27 06:34:18 -0300

    Update ObjTabbedPane.java

--- java/2°ano/4bim/prjLoja/Loja/ObjTabbedPane.java
+++ java/2°ano/4bim/prjLoja/Loja/ObjTabbedPane.java
@@ -1,24 +1,27 @@
+package Model;
+
 import javax.swing.*;
-import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.SQLException;
+import java.util.List;
+import DAO.ProdutoDao;
+import DAO.PedidoDao;
+import Model.Produto;
+import Model.Pedido;
 
-public class ObjJTabbedPane extends JDialog{
+public class ObjTabbedPane extends JDialog{
 
-		private JLabel lbNomeProduto; //PRODUTO
-		private JTextField txNomeProduto;
-		private JLabel lbValorProduto;
-		private JTextField txValorProduto;
-		private JButton btCadastrar;
-		private JLabel lbNomeCliente; //CLIENTE
-		private JTextField txNomeCliente;
-		private JLabel lbcpfCliente;
-		private JTextField txcpfCliente;
-		private JButton btCadastrarCliente;
-		private JButton btCadastrarPedido; //PEDIDO
+		private JButton btConsultaCategoria; //PRODUTO
+		private JButton btConsultaProduto;
+		private JButton btConsultaPedido; //PEDIDO
+		private JButton btConsultaItensPedido;
+	  /*private JButton btConsultaPedido; //CLIENTE
+		private JButton btConsultaItensPedido;*/
+		
 	
-	public ObjJTabbedPane() {
-	 this.setTitle("Cadastro de Clientes");
+	public ObjTabbedPane() {
+	 this.setTitle("Consultar Banco");
 	 this.setModal(true);                       
 	 this.setSize(450,380);
 	 this.setResizable(false);                       
@@ -38,31 +41,22 @@ public class ObjJTabbedPane extends JDialog{
 	  JPanel painel1 = new JPanel();
 	  painel1.setLayout(null);
 	  
-	  //Nome Produto
-	  lbNomeProduto = new JLabel("Nome:");
-	  lbNomeProduto.setBounds(10,15,75,20);     
-	  painel1.add(lbNomeProduto);   
-	     
-	  txNomeProduto = new JTextField();
-	  txNomeProduto.setBounds(85, 15, 150, 20);
-	  painel1.add(txNomeProduto);
-	  
-	  //Valor Produto
-	  lbValorProduto = new JLabel("Valor em R$:");
-	  lbValorProduto.setBounds(10,55,75,20);     
-	  painel1.add(lbValorProduto);   
-	     
-	  txValorProduto = new JTextField();
-	  txValorProduto.setBounds(85, 55, 150, 20);
-	  painel1.add(txValorProduto);
-	  
-	  //BOTÃO CADASTRAR
-	  btCadastrar = new JButton("Cadastrar");
-	  btCadastrar.setBounds(15,150,100,20); // Posição x(horizontal),y(vertical) e largura e altura
-	  painel1.add(btCadastrar);
+	  //BOTÃO CONSULTA CATEGORIA
+	  btConsultaCategoria = new JButton("Consulta Categoria");
+	  btConsultaCategoria.setBounds(15,35,175,20); // Posição x(horizontal),y(vertical) e largura e altura
+	  painel1.add(btConsultaCategoria);
 	
-	  EventoBotao evb = new EventoBotao();
-	  btCadastrar.addActionListener(evb);
+	  EventoBotaoCategoria evbC = new EventoBotaoCategoria();
+	  btConsultaCategoria.addActionListener(evbC);
+	  
+	  //BOTÃO CONSULTA PRODUTO
+	  btConsultaProduto = new JButton("Consulta Produtos");
+	  btConsultaProduto.setBounds(15,55,175,20); // Posição x(horizontal),y(vertical) e largura e altura
+	  painel1.add(btConsultaProduto);
+	
+	  EventoBotaoProduto evbP = new EventoBotaoProduto();
+	  btConsultaProduto.addActionListener(evbP);
+	  
 	  //add painel
 	  tabbedpane.addTab("Produto", null,painel1,"Primeiro Painel");
 	  
@@ -73,32 +67,6 @@ public class ObjJTabbedPane extends JDialog{
 	  painel2.setLayout(null);
 	  painel2.setBackground(Color.WHITE);  
 	  
-	  //Nome Cliente
-	  lbNomeCliente = new JLabel("Nome:");
-	  lbNomeCliente.setBounds(10,15,50,20);     
-	  painel2.add(lbNomeCliente);   
-	     
-	  txNomeCliente = new JTextField();
-	  txNomeCliente.setBounds(60, 15, 150, 20);
-	  painel2.add(txNomeCliente);
-	  
-	  //CPF Cliente
-	  lbcpfCliente = new JLabel("CPF:");
-	  lbcpfCliente.setBounds(10,55,50,20);     
-	  painel2.add(lbcpfCliente);   
-	     
-	  txcpfCliente = new JTextField();
-	  txcpfCliente.setBounds(60, 55, 150, 20);
-	  painel2.add(txcpfCliente);
-	  
-	  //BOTÃO CADASTRAR
-	  btCadastrarCliente = new JButton("Cadastrar");
-	  btCadastrarCliente.setBounds(15,150,100,20); // Posição x(horizontal),y(vertical) e largura e altura
-	  painel2.add(btCadastrarCliente);
-	
-	  EventoBotaoCliente evbC = new EventoBotaoCliente();
-	  btCadastrarCliente.addActionListener(evbC);
-	  
 	  //add painel
 	  tabbedpane.addTab("Cliente", null,painel2,"Segundo Painel");  
 	  
@@ -107,54 +75,87 @@ public class ObjJTabbedPane extends JDialog{
 	  //PAINEL PEDIDO
 	  JPanel painel3 = new JPanel(); 
 	  painel3.setLayout(null);
-	  
-	  //Nome Cliente
-	  lbNomeCliente = new JLabel("Nome Cliente:");
-	  lbNomeCliente.setBounds(10,15,85,20);     
-	  painel3.add(lbNomeCliente);   
-	     
-	  txNomeCliente = new JTextField();
-	  txNomeCliente.setBounds(95, 15, 150, 20);
-	  painel3.add(txNomeCliente);
-	  
-	  //Nome Produto
-	  lbNomeProduto = new JLabel("Nome Produto:");
-	  lbNomeProduto.setBounds(10,55,85,20);     
-	  painel3.add(lbNomeProduto);   
-	     
-	  txNomeProduto = new JTextField();
-	  txNomeProduto.setBounds(95, 55, 150, 20);
-	  painel3.add(txNomeProduto);
 	
-	  //BOTÃO CADASTRAR PEDIDO
-	  btCadastrarPedido = new JButton("Cadastrar Pedido");
-	  btCadastrarPedido.setBounds(15,150,200,20); // Posição x(horizontal),y(vertical) e largura e altura
-	  painel3.add(btCadastrarPedido);
+	  //BOTÃO CONSULTA PEDIDO
+	  btConsultaPedido = new JButton("Consulta Pedido");
+	  btConsultaPedido.setBounds(15,35,175,20); // Posição x(horizontal),y(vertical) e largura e altura
+	  painel3.add(btConsultaPedido);
 	
-	  EventoBotaoPedido evbP = new EventoBotaoPedido();
-	  btCadastrarPedido.addActionListener(evbP);
+	  EventoBotaoPedido evbPE = new EventoBotaoPedido();
+	  btConsultaPedido.addActionListener(evbPE);
 	  
+	  //BOTÃO ITENS PEDIDO
+	  btConsultaItensPedido = new JButton("Consulta Itens Pedido");
+	  btConsultaItensPedido.setBounds(15,55,175,20); // Posição x(horizontal),y(vertical) e largura e altura
+	  painel3.add(btConsultaItensPedido);
+	
+	  EventoBotaoItensPedido evbIPE = new EventoBotaoItensPedido();
+	  btConsultaItensPedido.addActionListener(evbIPE);
+	
 	  //add painel
 	  tabbedpane.addTab("Pedido", null,painel3,"Terceiro Painel");  
 	
 	  janelaCalc.add(tabbedpane);   
   }
-   
-   private class EventoBotao implements ActionListener{ //BOTÃO PRIMEIRA GUIA
+	//BOTÕES PRIMEIRA GUIA PRODUTOS
+   private class EventoBotaoCategoria implements ActionListener{ // CATEGORIA PRODUTO
        public void actionPerformed(ActionEvent event){
-    	   	JOptionPane.showMessageDialog(null,"Nome Produto: "+txNomeProduto.getText()+"\n"+"Valor Produto: "+txValorProduto.getText());
-        }   
+    	   		List<Produto> categoriaProduto;
+				try {
+					categoriaProduto = new ProdutoDao().getListaCategoria();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					throw new RuntimeException("Erro ao consultar tabela", e);
+				} //Atributo que Carrega o SELECT ProdutoDao
+    	   		for(Produto pro: categoriaProduto) { //Exibe os itens da Lista
+    	   			JOptionPane.showMessageDialog(null,"Id Categoria: "+pro.getIdCategoriaProduto()+" "+"Nome Categoria: "+pro.getNomeCategoriaProduto());
+    	   		}
+       }   
    } 
    
-   private class EventoBotaoCliente implements ActionListener{ //BOTÃO SEGUNDA GUIA
+   private class EventoBotaoProduto implements ActionListener{ // PRODUTO
        public void actionPerformed(ActionEvent event){
-    	   	JOptionPane.showMessageDialog(null,"Nome Cliente: "+txNomeCliente.getText()+"\n"+"CPF Cliente: "+txcpfCliente.getText());
-        }   
+    	   		List<Produto> produtos;
+				try {
+					produtos = new ProdutoDao().getListaProduto();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					throw new RuntimeException("Erro ao consultar tabela", e);
+				} //Atributo que Carrega o SELECT ProdutoDao
+	   			for(Produto pro: produtos) { //Exibe os itens da Lista
+	   				JOptionPane.showMessageDialog(null,"Nome: "+pro.getNomeProduto()+" "+"Valor: "+pro.getValorProduto()+" "+"Quantidade: "+pro.getQuantidadeProduto()+" "+"Id Categoria: "+pro.getIdCategoriaProduto());
+	   			}
+       }   
+   }
+	//BOTÕES SEGUNDA GUIA PEDIDO
+   private class EventoBotaoPedido implements ActionListener{ // PEDIDO
+       public void actionPerformed(ActionEvent event){	
+    	   		List<Pedido> pedidos;
+				try {
+					pedidos = new PedidoDao().getListaPedido();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					throw new RuntimeException("Erro ao consultar tabela", e);
+				} //Atributo que Carrega o SELECT PedidoDao
+    	   		for(Pedido pro: pedidos) { //Exibe os itens da Lista
+    	   			JOptionPane.showMessageDialog(null,"Id: "+pro.getIdPedido()+" "+"Data: "+pro.getDataPedido()+" "+"Valor: "+pro.getValorPedido()+" "+"DataEntrega: "+pro.getDataEntregaPedido());
+    	   		}
+       }   
    }
    
-   private class EventoBotaoPedido implements ActionListener{ //BOTÃO TERCEIRA GUIA
+   private class EventoBotaoItensPedido implements ActionListener{ // ITENS PEDIDO
        public void actionPerformed(ActionEvent event){
-    	   	JOptionPane.showMessageDialog(null,"Nome Cliente: "+txNomeCliente.getText()+"\n"+"Nome Produto: "+txNomeProduto.getText());
-        }   
+    	   		List<Pedido> itensPedido;
+				try {
+					itensPedido = new PedidoDao().getListaItensPedido();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					throw new RuntimeException("Erro ao consultar tabela", e);
+				} //Atributo que Carrega o SELECT ProdutoDao
+    	   		for(Pedido pro: itensPedido) { //Exibe os itens da Lista
+    	   			JOptionPane.showMessageDialog(null,"Quantidade: "+pro.getQuantidadeItensPedido()+" "+"id Pedido: "+pro.getIdPedido()+" "+"id Produto: "+pro.getIdProduto());
+    	   		}
+       }   
    }
+   
 }
