f535b87258e47fbde6c04c9a303e5aaf9e81a485
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-02-14 10:52:35 -0300

    Create simples.java

---
+++ TPA/3°ano/simples.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Ler data atual e data nascimento e retornar idade
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Insira o ano que você nasceu: ");
+		int anoNas = ler.nextInt();
+		System.out.println("Insira o ano atual: ");
+		int anoAtual = ler.nextInt();
+		int idade = anoAtual -anoNas;
+		
+		System.out.println("Você tem: " + idade + " anos");
+	}
+}
