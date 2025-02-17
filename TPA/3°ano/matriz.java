11c244af53bf217ecb3260a28a755b7f4e9e40df
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-02-17 19:51:18 -0300

    Update matriz.java

--- TPA/3°ano/matriz.java
+++ TPA/3°ano/matriz.java
@@ -1,17 +1,29 @@
-import java.util.Random;
 
-public class App {
+public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Matriz com números aleatórios
 		int matriz[][] = new int[5][5];
-		Random aleatorio = new Random();
+        int max = 9;
+        int min = 0;
+        int range = max - min + 1;
 		
-		for (int x = 0; x > 5; x++) {
-			for (int y =0; y > 5; y++) {
-				
+		for (int x = 0; x < 5; x++) {
+			for (int y =0; y < 5; y++) {
+				int aleatorio = (int)(Math.random() * range) + min;
+				System.out.println(aleatorio);
+				matriz[x][y] = aleatorio;
 			}
 		}
+		
+		System.out.println("\nMATRIZ\n");
+		
+		for (int x = 0; x < 5; x++) {
+			for (int y =0; y < 5; y++) {
+				System.out.print(" " + matriz[x][y] + " |");
+			}
+			System.out.println();
+		}
 	}
-
 }
