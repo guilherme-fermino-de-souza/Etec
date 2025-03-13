dddf19d962cb00ea5aa81c78c854775723bdb719
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-03-13 07:54:29 -0300

    Create dicionario.py

---
+++ QualidadeTesteSoftware/1°bim/exercicios/dicionario.py
@@ -0,0 +1,3 @@
+meuDicionario = {'chave01': valorChave1, 'chave02': valorChave2, 'chave03': valorChave3, 'chave04': valorChave4}
+
+meuDicionario = {'chave01': 30, 'chave02': 'textinho', 'chave03': 5.1, 'chave04': True}
