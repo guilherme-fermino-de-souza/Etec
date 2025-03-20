3080676486e965af456c53c1b0407c3e2fdcdabb
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-03-20 08:40:04 -0300

    Update cod01.py

--- QualidadeTesteSoftware/1°bim/links-externos/cod01.py
+++ QualidadeTesteSoftware/1°bim/links-externos/cod01.py
@@ -35,12 +35,11 @@ print (dicRequisica)
 
 
 
-dias = input('Digite o período em dias a consultar: ')
+dias = input('Digite o período em dias a consultar no mês de março (ex: 12; 2; 30): ')
 
 for i in range(1, dias):
     i += 1
     requisic = requests.get(link)
     dicRequisic = requisic.json()
-    link = f"https://economia.awesomeapi.com.br/all/{dias}"
+    link = f"view-source:https://api.tempo.com/index.php?api_lang=br&localidad=12996&affiliate_id=tk99un15usak&v=3.0/202503{dias}"
     print('¨¨'*40)
-  
