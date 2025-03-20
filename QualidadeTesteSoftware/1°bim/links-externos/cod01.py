f688709c6d43f3650e1e29eec247fd8751166cb1
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-03-20 08:27:26 -0300

    Create cod01.py

---
+++ QualidadeTesteSoftware/1°bim/links-externos/cod01.py
@@ -0,0 +1,37 @@
+import requests
+
+"""
+cep = input('Digite o número do seu CEP:')
+
+link = f"http://viacep.com.br/ws/{cep}/json/"
+
+requisicao = requests.get(link)
+dicRequisicao = requisicao.json()
+print(dicRequisicao)
+
+estadobuscador = dicRequisicao['uf']
+logradourobuscador = dicRequisicao['logradouro']
+bairrobuscador = dicRequisicao['bairro']
+cidadebuscador = dicRequisicao['localidade']
+
+print(estadobuscador)
+print(logradourobuscador)
+print(bairrobuscador)
+print(cidadebuscador)
+"""
+
+
+
+"""
+moeda = input('Digite:   USD || ARS || ETH:')
+
+link = f"https://economia.awesomeapi.com.br/all/{moeda}"
+
+requisica = requests.get(link)
+dicRequisica = requisica.json()
+print (dicRequisica)
+
+"""
+
+
+
