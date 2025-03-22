4ab102f40b7c307a2a1a390f300c8c632a01d4bc
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-03-22 11:36:00 -0300

    Update cod01.py

--- QualidadeTesteSoftware/1°bim/links-externos/cod01.py
+++ QualidadeTesteSoftware/1°bim/links-externos/cod01.py
@@ -1,6 +1,6 @@
 import requests
 
-"""
+""" Moeda (Feito)
 cep = input('Digite o número do seu CEP:')
 
 link = f"http://viacep.com.br/ws/{cep}/json/"
@@ -34,12 +34,29 @@ print (dicRequisica)
 """
 
 
+""" Clima () """
+print("Clima Hoje: ")
+link = f"https://api.tempo.com/index.php?api_lang=br&localidad=12996&affiliate_id=tk99un15usak&v=3.0"
+requisic = requests.get(link)
+dicRequisic = requisic.json()
+print(f"Data: {dicRequisic['location']}")
+print(f"Dia Semana: {dicRequisic}")
+print(f"Descrição Tempo: {dicRequisic}")
+print(f"Temperatura Mínima: {dicRequisic['location']}")
+print(f"Temperatura Máxima: {dicRequisic['location']}")
+print(f"Velocidade Vento: {dicRequisic['location']}")
+print(f"Humidade Relativa Do Ar: {dicRequisic['location']}")
+print(f"Hora Nascer Do Sol: {dicRequisic['location']}")
+print(f"Hora do pôr do Sol: {dicRequisic['location']}")
+print(f"Fase da Lua: {dicRequisic['location']}")
+print(f"Localização: {dicRequisic['location']}")
 
-dias = input('Digite o período em dias a consultar no mês de março (ex: 12; 2; 30): ')
 
-for i in range(1, dias):
-    i += 1
-    requisic = requests.get(link)
-    dicRequisic = requisic.json()
-    link = f"view-source:https://api.tempo.com/index.php?api_lang=br&localidad=12996&affiliate_id=tk99un15usak&v=3.0/202503{dias}"
-    print('¨¨'*40)
+
+""" AdviceSlipe (Feito) 
+print("Slipe Advice: ")
+link = "https://api.adviceslip.com/advice"
+requisicao = requests.get(link)
+print(requisicao.json()['slip']['id'])
+print(requisicao.json()['slip']['advice'])
+"""
