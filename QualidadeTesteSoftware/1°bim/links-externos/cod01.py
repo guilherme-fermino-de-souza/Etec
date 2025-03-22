dba54140a7a3bee9413129075620559cae8db60f
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-03-22 11:44:01 -0300

    Update cod01.py

--- QualidadeTesteSoftware/1°bim/links-externos/cod01.py
+++ QualidadeTesteSoftware/1°bim/links-externos/cod01.py
@@ -34,24 +34,14 @@ print (dicRequisica)
 """
 
 
-""" Clima () """
+""" Clima (Feito) 
 print("Clima Hoje: ")
 link = f"https://api.tempo.com/index.php?api_lang=br&localidad=12996&affiliate_id=tk99un15usak&v=3.0"
 requisic = requests.get(link)
 dicRequisic = requisic.json()
-print(f"Data: {dicRequisic['location']}")
-print(f"Dia Semana: {dicRequisic}")
-print(f"Descrição Tempo: {dicRequisic}")
-print(f"Temperatura Mínima: {dicRequisic['location']}")
-print(f"Temperatura Máxima: {dicRequisic['location']}")
-print(f"Velocidade Vento: {dicRequisic['location']}")
-print(f"Humidade Relativa Do Ar: {dicRequisic['location']}")
-print(f"Hora Nascer Do Sol: {dicRequisic['location']}")
-print(f"Hora do pôr do Sol: {dicRequisic['location']}")
-print(f"Fase da Lua: {dicRequisic['location']}")
 print(f"Localização: {dicRequisic['location']}")
-
-
+print(f"Dados: {dicRequisic['day']}")
+"""
 
 """ AdviceSlipe (Feito) 
 print("Slipe Advice: ")
